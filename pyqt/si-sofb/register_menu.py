2767cc17019883c23d44f19ecb865aad14f260d3
fernando <fernandohds564@gmail.com>
2017-06-16 11:28:49 -0300

    Bug fixes in register_menu.

--- pyqt/si-sofb/register_menu.py
+++ pyqt/si-sofb/register_menu.py
@@ -6,7 +6,8 @@ from selection_matrix import NR_BPMs
 
 class RegisterMenu(QMenu):
     DEFAULT_DIR = '/home/fernando'
-    EXTENSION = 'Sirius Orbit Files (*.siorb)'
+    EXT = '.siorb'
+    EXT_FLT = 'Sirius Orbit Files (*.siorb)'
 
     new_orbitx_signal = pyqtSignal(_np.ndarray)
     new_orbity_signal = pyqtSignal(_np.ndarray)
@@ -32,7 +33,7 @@ class RegisterMenu(QMenu):
     def orbx(self): return self._orbx.copy()
 
     @property
-    def orbx(self): return self._orbx.copy()
+    def orby(self): return self._orby.copy()
 
     def setupUi(self):
         act = self.addAction('&Load Orbit')
@@ -44,23 +45,14 @@ class RegisterMenu(QMenu):
         act.triggered.connect(self._register_orbit('orb'))
         act = menu.addAction('Re&ference Orbit')
         act.triggered.connect(self._register_orbit('ref'))
+        act = menu.addAction('&Golden Orbit')
+        act.triggered.connect(self._register_orbit('gol'))
         act = menu.addAction('&Offline Orbit')
         act.triggered.connect(self._register_orbit('off'))
         act = self.addAction('&Reset Orbit')
         act.triggered.connect(self._reset_orbit)
         act = self.addAction('&Save Registered Orbit')
         act.triggered.connect(self._save_orbit)
-        #### maybe will be usefull when implementing graphics selection
-        # menu = self.addMenu('&Save Orbit')
-        # act = menu.addAction('Absolute')
-        # act.triggered.connect(self._save_orbit(0))
-        # for i in range(1,10):
-        #     act = menu.addAction('as diff to Reg. &'+str(i))
-        #     act.triggered.connect(self._save_orbit(i))
-        #     regLb = getattr(self.main_window,'LB_Register'+str(i))
-        #     self.new_string_signal.connect(regLb.setText)
-        # orbx = self.main_window.SOFB_orbitx - sub_orbx
-        # orby = self.main_window.SOFB_orbity - sub_orby
 
     def _reset_orbit(self):
         ze = _np.zeros(self._orbx.shape)
@@ -69,15 +61,21 @@ class RegisterMenu(QMenu):
     def _register_orbit(self,flag):
         def register(self):
             if flag == 'orb':
-                orbx = self.main_window.SOFB_orbx
-                orby = self.main_window.SOFB_orby
+                orbx = self.main_window.PV_SOFBOrbitXMon.value
+                orby = self.main_window.PV_SOFBOrbitYMon.value
             elif flag == 'ref':
-                orbx = self.main_window.SOFB_reforbx
-                orby = self.main_window.SOFB_reforby
+                orbx = self.main_window.PV_SOFBOrbitRefXRB.value
+                orby = self.main_window.PV_SOFBOrbitRefYRB.value
             elif flag == 'off':
-                orbx = self.main_window.SOFB_offlineorbx
-                orby = self.main_window.SOFB_offlineorby
-            self._update_and_emit(orbx,orby,'', 'Orbit Registered.')
+                orbx = self.main_window.PV_SOFBOfflineOrbitXRB.value
+                orby = self.main_window.PV_SOFBOfflineOrbitYRB.value
+            elif flag == 'gol':
+                orbx = self.main_window.PV_SOFBGoldenOrbitXRB.value
+                orby = self.main_window.PV_SOFBGoldenOrbitYRB.value
+            else:
+                return
+            if orbx is not None and orby is not None:
+                self._update_and_emit(orbx,orby,'', 'Orbit Registered.')
         return register
 
     def _save_orbit(self,register):
@@ -86,16 +84,16 @@ class RegisterMenu(QMenu):
         header = '# ' + _datetime.datetime.now().strftime('%Y/%M/%d-%H:%M:%S') + '\n'
         filename = QFileDialog.getSaveFileName(caption='Define a File Name to Save the Orbit',
                                                directory=self.last_dir,
-                                               filter=self.EXTENSION)
+                                               filter=self.EXT_FLT)
         fname = filename[0]
-        fname += '' if fname.endswith(self.EXTENSION) else self.EXTENSION
+        fname += '' if fname.endswith(self.EXT) else self.EXT
         _np.savetxt(fname,_np.vstack([self.orbx,self.orby]).T, header=header)
         self._update_and_emit(orbx,orby,fname, 'Orbit Saved: ')
 
     def _load_orbit(self):
         filename = QFileDialog.getOpenFileName(caption='Select an Orbit File.',
                                                directory= self.last_dir,
-                                               filter= self.EXTENSION)
+                                               filter= self.EXT_FLT)
         if not filename[0]: return
         orbx, orby = _np.loadtxt(filename,unpack=True)
         self._update_and_emit(orbx,orby,filename[0], 'Orbit Loaded: ' )
