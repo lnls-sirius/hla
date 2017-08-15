8ff766747c2f922709614a2276d7fb1480cb0bb2
ximenes <xresende@gmail.com>
2017-08-15 11:37:11 -0300

    Rename HLA scripts
    - and interchange default colour of interlock leds

--- pyqt-apps/scripts/bo_config_manager.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pydm import PyDMApplication
-from siriushla.as_config_manager import ConfigManagerWindow
-
-app = PyDMApplication(None, sys.argv)
-window = ConfigManagerWindow('BoStrengthPvs')
-window.show()
-sys.exit(app.exec_())
--- pyqt-apps/scripts/si_ma_control.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pydm import PyDMApplication
-from siriushla.as_ma_control import SiriusMagnetControlWindow
-
-
-app = PyDMApplication(None, sys.argv)
-window = SiriusMagnetControlWindow()
-window.show()
-sys.exit(app.exec_())
