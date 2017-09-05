f8a92bb58307ca96ac512810ee8916ea211de511
ximenes <xresende@gmail.com>
2017-09-05 15:41:31 -0300

    Generalise siriushla/util.py
    read Qt style from .sirius-hla-style.css in $HOME, if it exists.

--- pyqt-apps/siriushla/util.py
+++ pyqt-apps/siriushla/util.py
@@ -2,11 +2,19 @@
 
 import siriushla.resources as _resources
 from pydm.PyQt.QtCore import QFile as _QFile
+import pathlib.Path as _Path
+import os as _os
 
 
 def set_style(app):
-    """Implement sirius-style.css as default Qt resource file for Sirius."""
-    stream = _QFile(':/style.css')
+    """Implement sirius-hla-style.css as default Qt resource file HLA."""
+    fname = _Path(_os.path.join(_Path.home(), '.sirius-hla-style.css'))
+    path = _Path(fname)
+    if path._is_file():
+        stream = _QFile(fname)
+    else:
+        stream = _QFile(':/sirius-hla-style.css')
+
     if stream.open(_QFile.ReadOnly):
         style = str(stream.readAll(), 'utf-8')
         stream.close()
