7fe87a35692a606ddd92e3a8616746302991efe2
ximenes <xresende@gmail.com>
2017-09-05 19:16:01 -0300

    Fix bug in util.py

--- pyqt-apps/siriushla/util.py
+++ pyqt-apps/siriushla/util.py
@@ -2,23 +2,25 @@
 
 import siriushla.resources as _resources
 from pydm.PyQt.QtCore import QFile as _QFile
-import pathlib.Path as _Path
+import pathlib as _pathlib
 import os as _os
 
 
-def set_style(app):
+def set_style(app, force_default=False):
     """Implement sirius-hla-style.css as default Qt resource file HLA."""
-    fname = _Path(_os.path.join(_Path.home(), '.sirius-hla-style.css'))
-    path = _Path(fname)
-    if path._is_file():
-        stream = _QFile(fname)
-    else:
+    if force_default:
         stream = _QFile(':/sirius-hla-style.css')
-
+    else:
+        fname = str(_os.path.join(_pathlib.Path.home(),
+                    '.sirius-hla-style.css'))
+        fpath = _pathlib.Path(fname)
+        if not fpath.is_file():
+            fname = ':/sirius-hla-style.css'
+        stream = _QFile(fname)
     if stream.open(_QFile.ReadOnly):
         style = str(stream.readAll(), 'utf-8')
         stream.close()
+        app.setStyleSheet(style)
     else:
-        print(stream.errorString())
-    app.setStyleSheet(style)
+        print('set_style: "{0}": {1}'.format(fname, stream.errorString()))
     _resources.qCleanupResources()
