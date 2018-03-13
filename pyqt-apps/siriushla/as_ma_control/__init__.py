4f72c43881315add3c1fa6601325d3ea2dc3498d
Guilherme <guilherme.prado@lnls.br>
2018-03-13 10:11:42 -0300

    Delete as_ma_control package

--- pyqt-apps/siriushla/as_ma_control/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Package containing widgets and windows to control magnets."""
-
-# from .TBMagnetControlWindow import TBMagnetControlWindow
-# from .BOMagnetControlWindow import BOMagnetControlWindow
-# from .TSMagnetControlWindow import TSMagnetControlWindow
-# from .SIMagnetControlWindow import SIMagnetControlWindow
