96a6d792f618fd8715be18fde2314098f1322e30
Ana <ana.clara@lnls.br>
2018-04-02 16:38:43 -0300

    Add SiriusImageView to qtplugins.

--- pyqt-apps/siriushla/widgets/qtplugins.py
+++ pyqt-apps/siriushla/widgets/qtplugins.py
@@ -7,6 +7,7 @@ from .QDoubleScrollBar import QDoubleScrollBar
 from .scrollbar import PyDMScrollBar
 from .state_button import PyDMStateButton
 from .windows import SiriusDialog, SiriusMainWindow
+from .image import SiriusImageView
 
 SIRIUS_CATEGORY = 'Sirius Widgets'
 
@@ -22,6 +23,10 @@ SiriusDialogPlugin = qtplugin_factory(SiriusDialog,
 PyDMLedPlugin = qtplugin_factory(PyDMLed, group=SIRIUS_CATEGORY)
 SiriusLedStatePlugin = qtplugin_factory(SiriusLedState, group=SIRIUS_CATEGORY)
 SiriusLedAlertPlugin = qtplugin_factory(SiriusLedAlert, group=SIRIUS_CATEGORY)
+
+# Image plugin
+SiriusImageViewPlugin = qtplugin_factory(SiriusImageView,
+                                         group=SIRIUS_CATEGORY)
 
 # Log Label
 PyDMLogLabelPlugin = qtplugin_factory(PyDMLogLabel,
