33f17ed808f8dfb907d613667d439a320ba64d8e
Ana <ana.clara@lnls.br>
2018-04-20 10:52:32 -0300

    Minor adaptations in Current and Lifetime HLA.

--- pyqt-apps/siriushla/as_ap_currlt/HLCurrentLifetime.py
+++ pyqt-apps/siriushla/as_ap_currlt/HLCurrentLifetime.py
@@ -36,10 +36,6 @@ class CurrLTWindow(SiriusMainWindow):
             self.setGraphBufferSize)
         self.centralwidget.spinBox_2.valueChanged.connect(
             self.setGraphTimeSpan)
-        self.centralwidget.checkBox.stateChanged.connect(
-            self.setCurrentTraceVisibility)
-        self.centralwidget.checkBox_2.stateChanged.connect(
-            self.setLifetimeTraceVisibility)
 
     def formatLifetime(self, value, **kws):
         """Format lifetime label."""
@@ -58,19 +54,11 @@ class CurrLTWindow(SiriusMainWindow):
     @pyqtSlot(int)
     def setGraphBufferSize(self, value):
         """Set graph buffer size."""
-        self.centralwidget.PyDMTimePlot.setBufferSize(value)
+        self.centralwidget.PyDMTimePlot_Current.setBufferSize(value)
+        self.centralwidget.PyDMTimePlot_Lifetime.setBufferSize(value)
 
     @pyqtSlot(int)
     def setGraphTimeSpan(self, value):
         """Set graph time span."""
-        self.centralwidget.PyDMTimePlot.setTimeSpan(float(value))
-
-    @pyqtSlot(int)
-    def setCurrentTraceVisibility(self, value):
-        """Set current trace visibility."""
-        self.centralwidget.PyDMTimePlot._curves[0].setVisible(value)
-
-    @pyqtSlot(int)
-    def setLifetimeTraceVisibility(self, value):
-        """Set lifetime trace visibility."""
-        self.centralwidget.PyDMTimePlot._curves[1].setVisible(value)
+        self.centralwidget.PyDMTimePlot_Current.setTimeSpan(float(value))
+        self.centralwidget.PyDMTimePlot_Lifetime.setTimeSpan(float(value))
