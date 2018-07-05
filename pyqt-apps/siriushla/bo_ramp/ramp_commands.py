0ffbf7dcc8a560669b651c40bf71e0077e1bf6c3
Ana <ana.clara@lnls.br>
2018-07-05 13:43:50 -0300

    Cleanup bo_ramp.ramp_commands code. Adapt to new ramp.conn venrsion.

--- pyqt-apps/siriushla/bo_ramp/ramp_commands.py
+++ pyqt-apps/siriushla/bo_ramp/ramp_commands.py
@@ -47,95 +47,79 @@ class RampCommands(QGroupBox):
             warn_msg = _MessageBox(
                 self, 'Failed to Upload',
                 'Command failed to set all PS\n'
-                'waveforms with timeout {}!'.format(timeout), 'Ok')
+                'waveforms!', 'Ok')
             warn_msg.exec_()
             return
-
-    def _initTiming(self):
-        if not self._conn_timing:
-            return
-        timeout = 0.5
-        configured_TI = self._conn_timing.cmd_init(timeout)
-        if not configured_TI:
-            warn_msg = _MessageBox(
-                self, 'Failed to Configure Timing',
-                'Failed to initialize timing\n'
-                'with timeout {}!'.format(timeout), 'Ok')
-            warn_msg.exec_()
-            return False
-        return True
 
     def _cycle(self):
         if not self._conn_magnets or not self._conn_timing:
             return
-        timeout = 0.5
-        configured_MA = self._conn_magnets.cmd_opmode_cycle(timeout)
+        configured_MA = self._conn_magnets.cmd_opmode_cycle()
         if not configured_MA:
             warn_msg = _MessageBox(
                 self, 'Failed to set OpMode',
                 'Command failed to set all PS\n'
-                'OpMode to Cycle with timeout {}!'.format(timeout), 'Ok')
+                'OpMode to Cycle!', 'Ok')
             warn_msg.exec_()
             return
 
-        init = self._initTiming(timeout)
-        if not init:
-            return
-
-        configured_TI = self._conn_timing.cmd_select_cycle(timeout)
+        configured_TI = self._conn_timing.cmd_select_cycle()
         if not configured_TI:
             warn_msg = _MessageBox(
                 self, 'Failed to Configure Timing',
                 'Command failed to configure timing\n'
-                'to cycle with timeout {}!'.format(timeout), 'Ok')
+                'to cycle!', 'Ok')
+            warn_msg.exec_()
+            return
+
+        pulse_TI = self._conn_timing.cmd_pulse()
+        if not pulse_TI:
+            warn_msg = _MessageBox(
+                self, 'Failed to Pulse Timing',
+                'Command failed to pulse timing\n'
+                'to cycle!', 'Ok')
             warn_msg.exec_()
             return
 
     def _start(self):
         if not self._conn_magnets or not self._conn_timing:
             return
-        timeout = 0.5
-        configured_MA = self._conn_magnets.cmd_opmode_rmpwfm(timeout)
+        configured_MA = self._conn_magnets.cmd_opmode_rmpwfm()
         if not configured_MA:
             warn_msg = _MessageBox(
                 self, 'Failed to set OpMode',
                 'Command failed to set all PS\n'
-                'OpMode to RampWfm with timeout {}!'.format(timeout), 'Ok')
+                'OpMode to RampWfm!', 'Ok')
             warn_msg.exec_()
             return
 
-        init = self._initTiming(timeout)
-        if not init:
-            return
-
-        configured_TI = self._conn_timing.cmd_select_ramp(timeout)
+        configured_TI = self._conn_timing.cmd_select_ramp()
         if not configured_TI:
             warn_msg = _MessageBox(
                 self, 'Failed to Configure Timing',
                 'Command failed to configure timing\n'
-                'to ramp with timeout {}!'.format(timeout), 'Ok')
+                'to ramp!', 'Ok')
             warn_msg.exec_()
             return
 
     def _stop(self):
         if not self._conn_magnets or not self._conn_timing:
             return
-        timeout = 0.5
-        configured_MA = self._conn_magnets.cmd_opmode_slowref(timeout)
+        configured_MA = self._conn_magnets.cmd_opmode_slowref()
         if not configured_MA:
             warn_msg = _MessageBox(
                 self, 'Failed to set OpMode',
                 'Command failed to set all PS\n'
-                'OpMode to RampWfm with timeout {}!'.format(timeout), 'Ok')
+                'OpMode to SlowRef!', 'Ok')
             warn_msg.exec_()
             return
 
-        configured_TI = self._conn_timing.cmd_select_stop(timeout)
+        configured_TI = self._conn_timing.cmd_select_stop()
         if not configured_TI:
             warn_msg = _MessageBox(
                 self, 'Failed to Configure Timing',
                 'Command failed to configure timing\n'
-                'to cycle with timeout {}!'.format(timeout), 'Ok')
+                'to stop pulsing!', 'Ok')
             warn_msg.exec_()
             return
 
