36839efeff821f3f7b11fabb48ed91f307ad86e1
Guilherme <guilherme.prado@lnls.br>
2018-07-23 10:57:47 -0300

    Fix import in config manager launcher script

--- pyqt-apps/scripts/sirius-hla-as-ap-config-manager.py
+++ pyqt-apps/scripts/sirius-hla-as-ap-config-manager.py
@@ -1,10 +1,11 @@
 """Lauch configuration database manager."""
 import sys
 
+from siriuspy.servconf.conf_service import ConfigService
 from siriushla import util
 from siriushla.sirius_application import SiriusApplication
 from siriushla.as_config_manager.config_server import \
-    ConfigurationManager, ConfigService
+    ConfigurationManager
 
 app = SiriusApplication()
 # util.set_style(app)
