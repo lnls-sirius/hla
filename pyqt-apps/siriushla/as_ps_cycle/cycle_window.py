96cb0bfa24d762c391f3999626704be237db7fdb
Guilherme <guilherme.prado@lnls.br>
2018-08-15 16:08:42 -0300

    Cycle window code cleanup

--- pyqt-apps/siriushla/as_ps_cycle/cycle_window.py
+++ pyqt-apps/siriushla/as_ps_cycle/cycle_window.py
@@ -5,11 +5,12 @@ import epics
 
 from pydm.PyQt.QtCore import pyqtSignal, QThread
 from pydm.PyQt.QtGui import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, \
-    QPushButton, QDialog, QLabel, QProgressBar, QMessageBox
+    QPushButton, QDialog, QLabel, QMessageBox
 
 from siriuspy.envars import vaca_prefix as VACA_PREFIX
 from siriushla.as_ps_cycle.cycle_status_list import CycleStatusList
 from siriushla.as_ps_cycle.magnets_tree import MagnetTree
+from siriushla.as_ps_cycle.progress_dialog import ProgressDialog
 
 
 class CycleWindow(QMainWindow):
@@ -52,7 +53,11 @@ class CycleWindow(QMainWindow):
         task1 = SetToCycle(self._cyclers, self)
         task2 = VerifyCycle(self._cyclers, self)
         task2.itemChecked.connect(self._check_cycling_status)
-        dlg = ProgressDialog('Setting magnets to cycle...', [task1, task2], self)
+
+        labels = ['Setting magnets...', 'Checking magnets...']
+        tasks = [task1, task2]
+
+        dlg = ProgressDialog(labels, tasks, self)
         ret = dlg.exec_()
         if ret == dlg.Rejected:
             return
@@ -213,98 +218,6 @@ class MagnetCycler:
         return False
 
 
-class ProgressDialog(QDialog):
-    """Progress dialog."""
-
-    canceled = pyqtSignal()
-
-    def __init__(self, label, task, parent=None):
-        """Constructor."""
-        super().__init__(parent)
-        self._label = label
-        self._task = task
-        self._setup_ui()
-
-    def _setup_ui(self):
-        self.layout = QVBoxLayout()
-
-        self.progress_bar = QProgressBar(self)
-        self.cancel = QPushButton('Cancel', self)
-
-        self.layout.addWidget(QLabel(self._label))
-        self.layout.addWidget(self.progress_bar)
-        self.layout.addWidget(self.cancel)
-
-        self.setLayout(self.layout)
-
-        # Set progress bar limit
-        self.progress_bar.setMinimum(0)
-        # Signals
-        self.cancel.pressed.connect(self.canceled.emit)
-        self.canceled.connect(lambda: self._exit_dlg(0))
-        self.progress_bar.valueChanged.connect(self._is_finished)
-
-        if hasattr(self._task, '__iter__'):
-            self._task[0].itemDone.connect(self.inc_value)
-            self.progress_bar.setMaximum(self._task[0].size())
-            for i in range(1, len(self._task)):
-                self._task[i].itemDone.connect(self.inc_value)
-                self._task[i - 1].finished.connect(self._task[i].start)
-                self.progress_bar.setMaximum(self.progress_bar.maximum() +
-                                             self._task[i].size())
-            self._task[-1].finished.connect(lambda: self._exit_dlg(1))
-        else:
-            self._task.itemDone.connect(self.inc_value)
-            self._task.finished.connect(lambda: self._exit_dlg(1))
-            self.progress_bar.setMaximum(self._task.size())
-
-    def _exit_dlg(self, result):
-        if hasattr(self._task, '__iter__'):
-            # for task in self._task:
-            self._task[-1].exit_task()
-            self._wait_task(self._task[-1])
-            self._task[-1].deleteLater()
-        else:
-            self._task.exit_task()
-            self._wait_task(self._task)
-            self._task.deleteLater()
-        if result == 1:
-            self.accept()
-        elif result == 0:
-            self.reject()
-
-    def _wait_task(self, task):
-        init = time.time()
-        try:
-            while task.isRunning():
-                time.sleep(0.1)
-                if time.time() - init > 10:
-                    self._exit_dlg(0)
-                    raise Exception('Thread will not leave')
-        except RuntimeError:
-            pass
-
-    def _is_finished(self):
-        if self.progress_bar.maximum() == self.progress_bar.value():
-            self._exit_dlg(0)
-
-    def set_value(self, value):
-        """Set progress bar value."""
-        self.progress_bar.setValue(value)
-
-    def inc_value(self):
-        """Increase value."""
-        self.progress_bar.setValue(self.progress_bar.value() + 1)
-
-    def exec_(self):
-        """Override."""
-        if hasattr(self._task, '__iter__'):
-            self._task[0].start()
-        else:
-            self._task.start()
-        return super().exec_()
-
-
 class CyclingDlg(QDialog):
     """Magnet cycle dialog."""
 
@@ -459,8 +372,9 @@ class WaitCycle(QThread):
                 break
         self.finished.emit()
 
-    def check_cycle_end(self, pvname, value, cb_info, **kwargs):
+    def check_cycle_end(self, pvname, value, **kwargs):
         """Check wether cycle finished."""
+        cb_info = kwargs['cb_info']
         state = self.pvs_state[pvname]
         if state == 0:
             if value == 0:
