191483046e9d3a3a8e806108752a98d07f2741ba
Ana <ana.clara@lnls.br>
2018-08-22 10:10:48 -0300

    Change SiriusWindows implementation to cleanup code. Fix flags handling.

--- pyqt-apps/siriushla/widgets/windows.py
+++ pyqt-apps/siriushla/widgets/windows.py
@@ -3,87 +3,52 @@ from PyQt5.QtWidgets import QMainWindow, QDialog
 from siriushla.sirius_application import SiriusApplication
 
 
-class SiriusMainWindow(QMainWindow):
-    """
-    SiriusMainWindow class.
+def _create_siriuswindow(qt_type):
+    """Create a _SiriusWindow that inherits from qt_type."""
+    class _SiriusWindow(qt_type):
+        """
+        Auxiliar _SiriusWindow class.
 
-    Parameters
-    ----------
-    parent : QWidget
-        The parent widget for the SiriusMainWindow
-    disconnect_when_hidden: bool
-        If True (default), close widgets connections when window is hidden and
-        establish widgets connections when shown. Else, establish connections
-        when first shown and close connections only on closeEvent.
-    """
+        Parameters
+        ----------
+        parent : QWidget
+            The parent widget for the SiriusMainWindow
+        disconnect_when_hidden: bool
+            If True (default), close widgets connections when window is hidden
+            and establish widgets connections when shown. Else, establish
+            connections when initialize and close connections only when closed.
+        """
 
-    def __init__(self, parent=None, disconnect_when_hidden=True, **kwargs):
-        """Reimplement constructor to include _is_connected flag."""
-        self._is_connected = False
-        self._disconnect_when_hidden = disconnect_when_hidden
-        super().__init__(parent, **kwargs)
+        def __init__(self, parent=None, disconnect_when_hidden=True, **kwargs):
+            """Class constructor."""
+            super().__init__(parent, **kwargs)
+            self._app = SiriusApplication.instance()
+            self._is_connected = False
+            self._disconnect_when_hidden = disconnect_when_hidden
 
-    def showEvent(self, ev):
-        """Reimplement showEvent to establish widget connections."""
-        if self._disconnect_when_hidden or not self._is_connected:
-            app = SiriusApplication.instance()
-            app.establish_widget_connections(self, False)
-            self._is_connected = True
-        super().showEvent(ev)
+        def showEvent(self, ev):
+            """Reimplement showEvent to establish widget connections."""
+            if self._disconnect_when_hidden or not self._is_connected:
+                self._app.establish_widget_connections(self, False)
+                self._is_connected = True
+            super().showEvent(ev)
 
-    def hideEvent(self, ev):
-        """Reimplement hideEvent to close widget connections."""
-        if self._disconnect_when_hidden:
-            app = SiriusApplication.instance()
-            app.close_widget_connections(self, False)
-        super().hideEvent(ev)
+        def hideEvent(self, ev):
+            """Reimplement hideEvent to close widget connections."""
+            if self._disconnect_when_hidden and self._is_connected:
+                self._app.close_widget_connections(self, False)
+                self._is_connected = False
+            super().hideEvent(ev)
 
-    def closeEvent(self, ev):
-        """Reimplement closeEvent to close widget connections."""
-        if not self._disconnect_when_hidden:
-            app = SiriusApplication.instance()
-            app.close_widget_connections(self, False)
-        super().closeEvent(ev)
+        def closeEvent(self, ev):
+            """Reimplement closeEvent to close widget connections."""
+            if self._is_connected:
+                self._app.close_widget_connections(self, False)
+                self._is_connected = False
+            super().closeEvent(ev)
+
+    return _SiriusWindow
 
 
-class SiriusDialog(QDialog):
-    """
-    SiriusDialog class.
-
-    Parameters
-    ----------
-    parent : QWidget
-        The parent widget for the SiriusMainWindow
-    disconnect_when_hidden: bool
-        If True (default), close widgets connections when window is hidden and
-        establish widgets connections when shown. Else, establish connections
-        when first shown and close connections only on closeEvent.
-    """
-
-    def __init__(self, parent=None, disconnect_when_hidden=True, **kwargs):
-        """Reimplement constructor to include _is_connected flag."""
-        self._is_connected = False
-        self._disconnect_when_hidden = disconnect_when_hidden
-        super().__init__(parent, **kwargs)
-
-    def showEvent(self, ev):
-        """Reimplement showEvent to establish widget connections."""
-        if self._disconnect_when_hidden or not self._is_connected:
-            app = SiriusApplication.instance()
-            app.establish_widget_connections(self, False)
-            self._is_connected = True
-        super().showEvent(ev)
-
-    def hideEvent(self, ev):
-        """Reimplement hideEvent to close widget connections."""
-        if self._disconnect_when_hidden:
-            app = SiriusApplication.instance()
-            app.close_widget_connections(self, False)
-        super().hideEvent(ev)
-
-    def closeEvent(self, ev):
-        """Reimplement closeEvent to close widget connections."""
-        if not self._disconnect_when_hidden:
-            app = SiriusApplication.instance()
-            app.close_widget_connections(self, False)
-        super().closeEvent(ev)
+SiriusMainWindow = _create_siriuswindow(QMainWindow)
+SiriusDialog = _create_siriuswindow(QDialog)
