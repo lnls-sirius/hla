6a5b350958ad8cee5427509c80f85fc0d74b3076
Guilherme <guilherme.prado@lnls.br>
2018-10-03 18:10:34 -0300

    Add PVConfigurationTableModel and its test

---
+++ pyqt-apps/siriushla/as_ap_pvsconfmgr/pv_configuration_model.py
@@ -0,0 +1,102 @@
+"""Model that gets the pvs field of configuration type."""
+from qtpy.QtCore import Qt, QAbstractTableModel
+from siriuspy.servconf.conf_types import get_config_type_template
+
+
+class PVConfigurationTableModel(QAbstractTableModel):
+    """Table model with PVs from a given type of configuration."""
+
+    def __init__(self, config_type=None, parent=None):
+        """Constructor.
+
+        config_type: configuration type name
+        connection: connection object
+        """
+        super().__init__(parent)
+        self._config_type = config_type
+        self._data = list()
+        self.setupModelData()
+
+    @property
+    def model_data(self):
+        """Data."""
+        return self._data
+
+    @property
+    def config_type(self):
+        """Configuration type."""
+        return self._config_type
+
+    @config_type.setter
+    def config_type(self, value):
+        self._config_type = value
+        self.setupModelData()
+
+    def rowCount(self, index):
+        """Return number of rows."""
+        return len(self._data)
+
+    def columnCount(self, index):
+        """Return number of columns."""
+        try:
+            return len(self._data[0])
+        except IndexError:
+            return 0
+
+    def data(self, index, role=Qt.DisplayRole):
+        """Return data at index."""
+        if not index.isValid():
+            return None
+
+        if role != Qt.DisplayRole:
+            return None
+
+        column = index.column()
+        row = index.row()
+
+        return self._data[row][column]
+
+    def headerData(self, section, orientation, role):
+        """Return header."""
+        if orientation == Qt.Horizontal:
+            if section == 0:
+                return 'PV'
+            elif section == 1:
+                return 'Value'
+        return super().headerData(section, orientation, role)
+
+    def setupModelData(self):
+        """Setup model data."""
+        if self._config_type is None:
+            return
+        self.beginResetModel()
+        config = get_config_type_template(self._config_type)
+        if 'pvs' in config:
+            self._data = config['pvs']
+        self.endResetModel()
+
+    def flags(self, index):
+        """Override to make cells editable."""
+        if not index.isValid():
+            return Qt.ItemIsEnabled
+        if index.column() == 1:
+            return Qt.ItemFlags(
+                QAbstractTableModel.flags(self, index) | Qt.ItemIsEditable)
+        return QAbstractTableModel.flags(self, index)
+
+    def setData(self, index, value, role=Qt.EditRole):
+        """Set data."""
+        if not index.isValid():
+            return False
+
+        column = index.column()
+        row = index.row()
+        
+        if column == 0:
+            return False
+
+        if column == 1:
+            self._data[row][column] = value
+            self.dataChanged.emit(index, index)
+
+        return True
