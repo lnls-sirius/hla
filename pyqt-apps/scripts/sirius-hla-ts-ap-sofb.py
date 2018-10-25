53e490331d59d5dadece53dc3424a713ec74aa4f
Fernando <fernandohds564@gmail.com>
2018-10-25 13:38:13 -0300

    ENH: add vaca_prefix as default value of --prefix in sofb windows

--- pyqt-apps/scripts/sirius-hla-ts-ap-sofb.py
+++ pyqt-apps/scripts/sirius-hla-ts-ap-sofb.py
@@ -4,13 +4,14 @@
 
 import sys
 import argparse as _argparse
+from siriuspy.envars import vaca_prefix
 from siriushla.sirius_application import SiriusApplication
 from siriushla.as_ap_sofb import MainWindow
 from siriushla import util
 
 
 parser = _argparse.ArgumentParser(description="Run SOFB HLA Interface.")
-parser.add_argument('-p', "--prefix", type=str, default='',
+parser.add_argument('-p', "--prefix", type=str, default=vaca_prefix,
                     help="Define the prefix for the PVs in the window.")
 args = parser.parse_args()
 
