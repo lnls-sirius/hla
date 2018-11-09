87abbb254ef99694036d83e926662741a182de81
Ana <ana.clara@lnls.br>
2018-11-09 12:04:10 -0200

    Add scripts to run transport lines ICT monitors.

--- pyqt-apps/siriushla/tl_ap_control/__init__.py
+++ pyqt-apps/siriushla/tl_ap_control/__init__.py
@@ -1,3 +1,5 @@
 from siriushla.tl_ap_control.HLTLControl import TLAPControlWindow
+from siriushla.tl_ap_control.ICT_monitor import ICTMonitoring
+from siriushla.tl_ap_control.Slit_monitor import SlitMonitoring
 
-__all__ = ('HLTLControl',)
+__all__ = ('HLTLControl', 'ICT_monitor', 'Slit_monitor')
