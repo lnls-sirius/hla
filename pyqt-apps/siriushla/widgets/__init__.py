3df1806b3baa4cac39962d34a76b5f846a84d471
Fernando <fernandohds564@gmail.com>
2019-02-11 09:46:21 -0200

    BUG: Remove SiriusScrnView from widgets list, since it will soon be considered as a package.

--- pyqt-apps/siriushla/widgets/__init__.py
+++ pyqt-apps/siriushla/widgets/__init__.py
@@ -1,17 +1,16 @@
-from siriushla.widgets.QLed import QLed
-from siriushla.widgets.led import PyDMLed, SiriusLedAlert, SiriusLedState
-from siriushla.widgets.led import PyDMLedMultiChannel
-from siriushla.widgets.led import PyDMLedMultiConnection
-from siriushla.widgets.log_label import PyDMLogLabel
-from siriushla.widgets.QDoubleScrollBar import QDoubleScrollBar
-from siriushla.widgets.scrollbar import PyDMScrollBar
-from siriushla.widgets.state_button import PyDMStateButton
-from siriushla.widgets.windows import SiriusMainWindow, SiriusDialog
-from siriushla.widgets.ledit_scrollbar import PyDMLinEditScrollbar
-from siriushla.widgets.loading_dialog import LoadingDialog
-from siriushla.widgets.scrn_view import SiriusScrnView
-from siriushla.widgets.signal_channel import SiriusConnectionSignal
-from siriushla.widgets.spinbox import SiriusSpinbox
-from siriushla.widgets.widget_factory import pydmwidget_factory
-from siriushla.widgets.spectrogram_view import SiriusSpectrogramView
-from siriushla.widgets.label import SiriusLabel
+from .QLed import QLed
+from .led import PyDMLed, SiriusLedAlert, SiriusLedState
+from .led import PyDMLedMultiChannel
+from .led import PyDMLedMultiConnection
+from .log_label import PyDMLogLabel
+from .QDoubleScrollBar import QDoubleScrollBar
+from .scrollbar import PyDMScrollBar
+from .state_button import PyDMStateButton
+from .windows import SiriusMainWindow, SiriusDialog
+from .ledit_scrollbar import PyDMLinEditScrollbar
+from .loading_dialog import LoadingDialog
+from .signal_channel import SiriusConnectionSignal
+from .spinbox import SiriusSpinbox
+from .widget_factory import pydmwidget_factory
+from .spectrogram_view import SiriusSpectrogramView
+from .label import SiriusLabel
