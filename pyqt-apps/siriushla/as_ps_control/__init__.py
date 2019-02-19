cda01d630b0e95c6dd2f34144525b15af4c0bc89
anacso17 <anacso1777@gmail.com>
2019-02-19 10:13:57 -0300

    ENH: Change PS and PM __init__.py modules to make widgets accessible.

--- pyqt-apps/siriushla/as_ps_control/__init__.py
+++ pyqt-apps/siriushla/as_ps_control/__init__.py
@@ -1,1 +1,7 @@
 """Package containing widgets and windows to control power supplies."""
+
+from .PSControlWindow import PSControlWindow
+from .PSDetailWindow import PSDetailWindow
+from .PSTabControlWindow import PSTabControlWindow
+from .PSTrimWindow import PSTrimWindow
+from .PSWidget import PulsedPSWidget, PSWidget, PulsedMAWidget, MAWidget
