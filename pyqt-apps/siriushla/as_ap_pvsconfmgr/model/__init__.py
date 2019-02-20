011c3761fe02665ed3ae00b6e7cc0fcfb16d4d12
Guilherme <guilherme.prado@lnls.br>
2019-02-20 09:56:42 -0300

    Add init files to model and dialog modules

---
+++ pyqt-apps/siriushla/as_ap_pvsconfmgr/model/__init__.py
@@ -0,0 +1,5 @@
+"""Pvs configuration manager models."""
+
+from .config_names_model import ConfigNamesModel
+from .config_types_model import ConfigTypeModel
+from .pv_configuration_model import PVConfigurationTableModel
