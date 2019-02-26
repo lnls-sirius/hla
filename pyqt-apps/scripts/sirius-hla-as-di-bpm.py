48c81b0ab75406c1aa48fc11cd205e010179b28a
anacso17 <anacso1777@gmail.com>
2019-02-26 16:40:14 -0300

    Change BPMs script to use SelectBPMs window instead of a BPMMain window.

--- pyqt-apps/scripts/sirius-hla-as-di-bpm.py
+++ pyqt-apps/scripts/sirius-hla-as-di-bpm.py
@@ -4,21 +4,38 @@
 
 import sys
 import argparse as _argparse
+from qtpy.QtWidgets import QInputDialog
 from siriuspy.envars import vaca_prefix
+from siriuspy.search.bpms_search import BPMSearch
 from siriushla.sirius_application import SiriusApplication
 from siriushla.widgets.windows import create_window_from_widget
-from siriushla.as_di_bpms import BPMMain
+from siriushla.as_di_bpms import SelectBPMs
 
 
 parser = _argparse.ArgumentParser(
     description="Run Interface of Specified BPM.")
-parser.add_argument('bpm', type=str, help='Select BPM.')
+parser.add_argument('sec', type=str, help='Select a section.')
 parser.add_argument('-p', "--prefix", type=str, default=vaca_prefix,
                     help="Define the prefix for the PVs in the window.")
 args = parser.parse_args()
 
+
 app = SiriusApplication()
-BPM = create_window_from_widget(BPMMain, 'BPMMainWindow', is_main=True)
-window = BPM(None, prefix=args.prefix, bpm=args.bpm)
+BPMsList = create_window_from_widget(SelectBPMs, 'BPMsList', is_main=True)
+
+if args.sec == 'SI':
+    sector, ok = QInputDialog.getItem(
+        None, 'Chose SI sector', 'Chose a sector number: ',
+        ['{:02}'.format(i) for i in range(1, 21)])
+    if ok:
+        bpms_names = BPMSearch.get_names(
+            filters={'sec': args.sec, 'sub': sector+'.*'})
+    else:
+        bpms_names = BPMSearch.get_names(
+            filters={'sec': args.sec, 'sub': '01.*'})
+else:
+    bpms_names = BPMSearch.get_names(filters={'sec': args.sec})
+window = BPMsList(None, prefix=args.prefix, bpm_list=bpms_names)
+window.setWindowTitle(args.sec + ' BPM List')
 window.show()
 sys.exit(app.exec_())
