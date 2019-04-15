86be59e28d8e7cfcd965b27956681510ace2e95a
anacso17 <anacso1777@gmail.com>
2019-04-15 13:59:33 -0300

    Change CycleStatusList to update list in every magnets property set

--- pyqt-apps/siriushla/as_ps_cycle/cycle_status_list.py
+++ pyqt-apps/siriushla/as_ps_cycle/cycle_status_list.py
@@ -12,7 +12,7 @@ class CycleStatusList(QListView):
     # OK = QIcon('icons8-checkmark.svg')
     # NOK = QIcon('icons8-delete.svg')
 
-    def __init__(self, magnets, parent=None):
+    def __init__(self, magnets=list(), parent=None):
         """Constructor."""
         super().__init__(parent)
         self._magnets = magnets
@@ -28,6 +28,7 @@ class CycleStatusList(QListView):
     @magnets.setter
     def magnets(self, magnets):
         self._magnets = magnets
+        self._setup_ui()
 
     def _setup_ui(self):
         self._model = QStandardItemModel(self)
