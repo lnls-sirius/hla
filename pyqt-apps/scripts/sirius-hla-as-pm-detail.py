ceb5c87443967c0d20c42118e2466c37c8768bab
Fernando <fernandohds564@gmail.com>
2019-07-18 15:34:55 -0300

    APP.ENH: Create a better solution for exception reporting in our windows.

--- pyqt-apps/scripts/sirius-hla-as-pm-detail.py
+++ pyqt-apps/scripts/sirius-hla-as-pm-detail.py
@@ -5,20 +5,14 @@
 import sys
 import argparse as _argparse
 from siriushla.sirius_application import SiriusApplication
+from siriushla.as_pm_control import PulsedMagnetDetailWindow
 
-try:
-    from siriushla.as_pm_control import PulsedMagnetDetailWindow
 
-    parser = _argparse.ArgumentParser(
-        description="Run Pulsed Magnet Detailed Control Interface.")
-    parser.add_argument("pmname", type=str, help="PM name.")
-    args = parser.parse_args()
+parser = _argparse.ArgumentParser(
+    description="Run Pulsed Magnet Detailed Control Interface.")
+parser.add_argument("pmname", type=str, help="PM name.")
+args = parser.parse_args()
 
-    app = SiriusApplication()
-    app.open_window(PulsedMagnetDetailWindow, parent=None, maname=args.pmname)
-    sys.exit(app.exec_())
-except:
-    app = SiriusApplication.instance()
-    if app is None:
-        app = SiriusApplication(None, sys.argv)
-    app.disclaimer()
+app = SiriusApplication()
+app.open_window(PulsedMagnetDetailWindow, parent=None, maname=args.pmname)
+sys.exit(app.exec_())
--- pyqt-apps/scripts/sirius-hla-tb-ap-posang.py
+++ pyqt-apps/scripts/sirius-hla-tb-ap-posang.py
@@ -5,23 +5,17 @@
 import sys as sys
 import argparse as argparse
 from siriushla.sirius_application import SiriusApplication
+from siriuspy.envars import vaca_prefix
+from siriushla.as_ap_posang.HLPosAng import ASAPPosAngCorr
 
-try:
-    from siriuspy.envars import vaca_prefix
-    from siriushla.as_ap_posang.HLPosAng import ASAPPosAngCorr
 
-    parser = argparse.ArgumentParser(
-                        description="Run TB PosAng HLA Interface.")
-    parser.add_argument(
-        '-p', "--prefix", type=str, default=vaca_prefix,
-        help="Define the prefix for the PVs in the window.")
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(
+    description="Run TB PosAng HLA Interface.")
+parser.add_argument(
+    '-p', "--prefix", type=str, default=vaca_prefix,
+    help="Define the prefix for the PVs in the window.")
+args = parser.parse_args()
 
-    app = SiriusApplication()
-    app.open_window(ASAPPosAngCorr, parent=None, prefix=args.prefix, tl='tb')
-    sys.exit(app.exec_())
-except:
-    app = SiriusApplication.instance()
-    if app is None:
-        app = SiriusApplication(None, sys.argv)
-    app.disclaimer()
+app = SiriusApplication()
+app.open_window(ASAPPosAngCorr, parent=None, prefix=args.prefix, tl='tb')
+sys.exit(app.exec_())
