fc9129dd2dfa5053f5177614e2fc6c7d579ee875
Fernando <fernandohds564@gmail.com>
2019-08-31 10:52:36 -0300

    BUG: Add try..except when setting PV in setter.py to avoid crash.

--- pyqt-apps/siriushla/misc/epics/task/setter.py
+++ pyqt-apps/siriushla/misc/epics/task/setter.py
@@ -11,8 +11,13 @@ class EpicsSetter(EpicsTask):
         if not self._quit_task:
             for i in range(len(self._pvs)):
                 self.currentItem.emit(self._pvs[i].pvname)
-                self._pvs[i].put(self._values[i])
-                time.sleep(self._delays[i])
+                try:
+                    self._pvs[i].put(self._values[i])
+                    time.sleep(self._delays[i])
+                except Exception:
+                    print(
+                        'PV ', self._pvs[i].pvname, 
+                        ' not set wit value: ', self._values[i])
                 self.itemDone.emit()
                 if self._quit_task:
                     break
