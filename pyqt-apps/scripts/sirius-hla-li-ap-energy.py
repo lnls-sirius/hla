0c752443f03a115f4d27ccee9406453b473da165
Fernando <fernandohds564@gmail.com>
2019-11-05 17:38:21 -0300

    ENH: Firs working version of the Linac energy measurement from IOC window.

--- pyqt-apps/scripts/sirius-hla-li-ap-energy.py
+++ pyqt-apps/scripts/sirius-hla-li-ap-energy.py
@@ -4,8 +4,10 @@
 
 import sys
 import argparse as _argparse
-from siriushla.sirius_application import SiriusApplication
+import qtawesome as qta
 from siriuspy.envars import vaca_prefix
+import siriushla.util as _util
+from siriushla.sirius_application import SiriusApplication
 from siriushla.as_ap_measure import EnergyMeasure
 from siriushla.widgets.windows import create_window_from_widget
 
@@ -18,7 +20,9 @@ parser.add_argument(
 args = parser.parse_args()
 
 app = SiriusApplication()
+icon = qta.icon(
+        'mdi.gauge', color=_util.get_appropriate_color('LI'))
 MyWindow = create_window_from_widget(
-    EnergyMeasure, title='Linac Energy Measure', is_main=True)
+    EnergyMeasure, title='Linac Energy Measure', is_main=True, icon=icon)
 app.open_window(MyWindow)
 sys.exit(app.exec_())
