a96431fc4ab8f6445a4679ad2f3c1aa714a4af21
Fernando <fernandohds564@gmail.com>
2019-11-27 15:32:15 -0300

    ENRBUT.ENH: Improve Energy button and add SI PS.

--- pyqt-apps/siriushla/as_ap_energybutton/set_energy.py
+++ pyqt-apps/siriushla/as_ap_energybutton/set_energy.py
@@ -1,8 +1,5 @@
 """."""
-from time import sleep
-import epics as _epics
-from siriuspy.search.ma_search import MASearch as _MASearch
-import siriuspy.magnet.data as _mdata
+from siriuspy.search import MASearch as _MASearch
 from siriuspy.namesys import SiriusPVName as _SiriusPVName
 
 
@@ -12,58 +9,23 @@ _TIMEOUT = 0.05
 def init_section(section):
     """."""
     dipole, magnets = _create_objects(section)
-    _create_pvs(dipole, magnets)
+    dipole, magnets = _create_pvs(dipole, magnets)
     return dipole, magnets
 
 
-def set_energy(energy, dipole, magnets):
+def _magfunc(maname):
     """."""
-    # store normalized values
-    fail_list = _store_normalized(dipole, magnets)
-    if fail_list:
-        return list(set(fail_list))
-    # set dipole energy
-    if not dipole['pv_sp'].connected:
-        fail_list += ['set:'+dipole['pv_sp'].pvname]
-        return list(set(fail_list))
-    dipole['pv_sp'].value = energy
-    dipole['sp_value'] = energy
-    sleep(0.5)
-    # restore normalized values
-    fail_list = _restore_normalized(dipole, magnets)
-    if fail_list:
-        return list(set(fail_list))
-    sleep(0.5)
-    # check values set
-    fail_list = _check_values(dipole, magnets)
-    return list(set(fail_list))
-
-
-def _magfunc(madata):
-    """."""
-    psnames = madata.psnames
-    return madata.magfunc(psnames[0])
-
-
-def _fail_list(dipole, magnets):
-    """."""
-    fail_list = []
-    if not dipole['pv_sp'].connected:
-        fail_list.append(dipole['pv_sp'].pvname)
-    for m in magnets:
-        if not m['pv_sp'].connected:
-            fail_list.append(m['pv_sp'].pvname)
-    return fail_list
+    func = _MASearch.conv_maname_2_magfunc(maname)
+    return list(func.values())[0]
 
 
 def _create_objects(section):
     """."""
     dipnames = {
-        'TB': 'TB-Fam:MA-B',
-        'BO': 'BO-Fam:MA-B',
-        'TS': 'TS-Fam:MA-B',
-        'SI': 'BO-Fam:MA-B1B2'
-    }
+        'TB': _SiriusPVName('TB-Fam:MA-B'),
+        'BO': _SiriusPVName('BO-Fam:MA-B'),
+        'TS': _SiriusPVName('TS-Fam:MA-B'),
+        'SI': _SiriusPVName('SI-Fam:MA-B1B2')}
     dipname = dipnames[section]
     manames = _MASearch.get_manames()
     magnets = []
@@ -71,80 +33,34 @@ def _create_objects(section):
         maname = _SiriusPVName(maname)
         if maname.sec != section:
             continue
-        if maname.dev == 'FC':
+        if maname.dev.startswith('FC'):
             continue
-        if maname.sub != 'Fam' and ('SD' in maname.dev or 'SF' in maname.dev):
+        if maname.sub != 'Fam' and maname.dev.startswith(('SF', 'SD')):
             continue
-        if maname.dis == 'PM':
-            continue
-        madata = _mdata.MAData(maname=maname)
-        mfunc = _magfunc(madata)
+        mfunc = _magfunc(maname)
         if mfunc == 'dipole':
+            dipole = {'maname': dipname, 'mafunc': mfunc}
             continue
-        dstruc = {
-            'maname': maname,
-            'madata': madata,
-        }
+        dstruc = {'maname': maname, 'mafunc': mfunc}
         magnets.append(dstruc)
-    dipole = {
-        'maname': dipname,
-        'madata': _mdata.MAData(maname=dipname),
-    }
     return dipole, magnets
 
 
 def _create_pvs(dipole, magnets):
     """."""
     maname = dipole['maname']
-    dipole['pv_sp'] = _epics.PV(maname + ':Energy-SP')
+    dip = [maname.substitute(propty_name='Energy', propty_suffix='SP')]
+    mags = set()
     for d in magnets:
         maname = d['maname']
-        madata = d['madata']
-        magf = _magfunc(madata)
+        magf = d['mafunc']
         if 'corrector' in magf:
-            d['pv_sp'] = _epics.PV(maname + ':Kick-SP', connection_timeout=_TIMEOUT)
+            mags.add(maname.substitute(
+                propty_name='Kick', propty_suffix='SP'))
         elif 'quadrupole' in magf:
-            d['pv_sp'] = _epics.PV(maname + ':KL-SP', connection_timeout=_TIMEOUT)
+            mags.add(maname.substitute(
+                propty_name='KL', propty_suffix='SP'))
         elif 'sextupole' in magf:
-            d['pv_sp'] = _epics.PV(maname + ':SL-SP', connection_timeout=_TIMEOUT)
-
-
-def _check_values(dipole, magnets):
-    fail_list = []
-    if abs(dipole['pv_sp'].value - dipole['sp_value']) > 1e-5:
-        # print(dipole['pv_sp'].pvname, dipole['pv_sp'].value, dipole['sp_value'])
-        fail_list.append(dipole['pv_sp'].pvname)
-    for m in magnets:
-        if abs(m['pv_sp'].value - m['sp_value']) > 1e-5:
-            # print(m['pv_sp'].pvname, m['pv_sp'].value, m['sp_value'])
-            fail_list.append(m['pv_sp'].pvname)
-    return fail_list
-
-
-def _store_normalized(dipole, magnets):
-    """."""
-    fail_list = _fail_list(dipole, magnets)
-    if fail_list:
-        return fail_list
-    for m in magnets:
-        if m['pv_sp'].connected:
-            m['sp_value'] = m['pv_sp'].value
-        else:
-            fail_list.append(m['pv_sp'].pvname)
-            m['sp_value'] = None
-    return fail_list
-
-
-def _restore_normalized(dipole, magnets):
-    """."""
-    fail_list = _fail_list(dipole, magnets)
-    if fail_list:
-        return fail_list
-    for m in magnets:
-        value = m['sp_value']
-        if value is not None and m['pv_sp'].connected:
-            m['pv_sp'].value = value
-            pass
-        else:
-            fail_list.append(m['pv_sp'].pvname)
-    return fail_list
+            mags.add(maname.substitute(
+                propty_name='SL', propty_suffix='SP'))
+    return dip, sorted(mags)
