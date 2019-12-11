36084ec9a9702c084af8152d0fc499909b695869
anacso17 <anacso1777@gmail.com>
2019-12-11 17:57:47 -0300

    ramp.MNT: replace use of stylesheet by palette to avoid crashing window in control room desktops

--- pyqt-apps/siriushla/bo_ap_ramp/bonormalized_edit.py
+++ pyqt-apps/siriushla/bo_ap_ramp/bonormalized_edit.py
@@ -6,7 +6,7 @@ from functools import partial as _part
 import numpy as _np
 
 from qtpy.QtCore import Qt, Signal, Slot
-from qtpy.QtGui import QKeySequence
+from qtpy.QtGui import QKeySequence, QPalette
 from qtpy.QtWidgets import QWidget, QGroupBox, QPushButton, QLabel, \
     QGridLayout, QScrollArea, QFormLayout, QCheckBox, QDoubleSpinBox, \
     QUndoStack, QUndoCommand, QHBoxLayout, QMessageBox, QMenuBar
@@ -395,11 +395,15 @@ class BONormEdit(SiriusMainWindow):
             self.norm_config.verify_syncronized()
         if not self.norm_config.synchronized:
             self.act_save.setEnabled(True)
-            self.label_name.setStyleSheet("color:red;")
+            pal = self.label_name.palette()
+            pal.setColor(QPalette.WindowText, Qt.red)
+            self.label_name.setPalette(pal)
             self.setToolTip("There are unsaved changes")
         else:
             self.act_save.setEnabled(False)
-            self.label_name.setStyleSheet("")
+            pal = self.label_name.palette()
+            pal.setColor(QPalette.WindowText, Qt.black)
+            self.label_name.setPalette(pal)
             self.setToolTip("")
 
     # ---------- strengths ----------
