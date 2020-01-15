7e6d4ab09dca2fd9717f8bc6654e23aa29ec6d3b
anacso17 <anacso1777@gmail.com>
2020-01-15 13:36:34 -0300

    PSCycle.ENH: make PV communications faster and move tasks to a separated file

--- pyqt-apps/siriushla/as_ps_cycle/cycle_window.py
+++ pyqt-apps/siriushla/as_ps_cycle/cycle_window.py
@@ -2,21 +2,18 @@
 
 import re as _re
 import time as _time
-from threading import Thread as _Thread, Lock as _Lock
-from datetime import datetime as _datetime
 from functools import partial as _part
 
 from qtpy.QtGui import QColor, QPalette
 from qtpy.QtCore import Signal, QThread, Qt, QTimer
 from qtpy.QtWidgets import QWidget, QGridLayout, QVBoxLayout, \
-    QPushButton, QLabel, QMessageBox, QLineEdit, QApplication, \
-    QListWidget, QListWidgetItem, QProgressBar, QGroupBox
+    QPushButton, QLabel, QMessageBox, QApplication, QGroupBox, \
+    QListWidget, QListWidgetItem, QProgressBar
 import qtawesome as qta
 
 from siriuspy.envars import vaca_prefix as VACA_PREFIX
 from siriuspy.namesys import Filter, SiriusPVName as PVName
-from siriuspy.cycle import get_psnames, \
-    Timing, PSCycler, LinacPSCycler, CycleController
+from siriuspy.cycle import get_psnames, Timing
 from siriuspy.search import PSSearch
 
 from siriushla.util import get_appropriate_color
@@ -25,9 +22,10 @@ from siriushla.widgets import SiriusMainWindow, \
 from siriushla.widgets.pvnames_tree import PVNameTree
 from siriushla.widgets.dialog import ProgressDialog, PSStatusDialog
 from siriushla.as_ps_control.PSDetailWindow import PSDetailWindow
+from siriushla.as_ps_cycle.tasks import CreateCyclers, VerifyPS, \
+    ZeroPSCurrent, ResetPSOpMode, RestoreTiming, PreparePSParams, \
+    PreparePSOpMode, PrepareTiming, Cycle
 
-_cyclers = dict()
-_lock = _Lock()
 
 errorcolor = QColor(255, 0, 0)
 warncolor = QColor(200, 200, 0)
@@ -77,11 +75,11 @@ class CycleWindow(SiriusMainWindow):
 
         # status
         gb_status = QGroupBox('Connection Status', self)
-        tipvs = [VACA_PREFIX + pv
-                 for pv in self._timing.get_pvnames_by_psnames()]
+        ti_ch = [VACA_PREFIX + name
+                 for name in self._timing.get_pvnames_by_psnames()]
         label_ticonn = QLabel('<h4>Timing</h4>', self,
                               alignment=Qt.AlignBottom | Qt.AlignHCenter)
-        self.ticonn_led = PyDMLedMultiConn(self, channels=tipvs)
+        self.ticonn_led = PyDMLedMultiConn(self, channels=ti_ch)
         self.ticonn_led.shape = 2
         label_psconn = QLabel('<h4>Selected Power Supplies</h4>', self,
                               alignment=Qt.AlignBottom | Qt.AlignHCenter)
@@ -213,9 +211,16 @@ class CycleWindow(SiriusMainWindow):
             self._allButtons_setEnabled(True)
             return False
 
+        create_task = CreateCyclers(parent=self, psnames=pwrsupplies)
+        dlg = ProgressDialog('Creating cycles...', create_task, self)
+        ret = dlg.exec_()
+        if ret == dlg.Rejected:
+            self._allButtons_setEnabled(True)
+            return False
+
         if subsystem == 'ps':
             self._ps_failed = set()
-            verify_task = VerifyPS(pwrsupplies, self)
+            verify_task = VerifyPS(parent=self, psnames=pwrsupplies)
             verify_task.itemDone.connect(self._get_ps_not_ready_2_cycle)
             dlg = ProgressDialog(
                 'Verifying power supplies initial state...', verify_task, self)
@@ -232,10 +237,16 @@ class CycleWindow(SiriusMainWindow):
                 return False
 
             self._is_preparing = 'ps_' + ppty
-            prepare_task = PreparePS(pwrsupplies, self._timing, ppty, self)
+            if ppty == 'params':
+                prepare_task = PreparePSParams(
+                    parent=self, psnames=pwrsupplies, timing=self._timing)
+            else:
+                prepare_task = PreparePSOpMode(
+                    parent=self, psnames=pwrsupplies, timing=self._timing)
         else:
             self._is_preparing = 'timing'
-            prepare_task = PrepareTiming(pwrsupplies, self._timing, self)
+            prepare_task = PrepareTiming(
+                parent=self, psnames=pwrsupplies, timing=self._timing)
 
         prepare_task.updated.connect(self._update_progress)
         duration = prepare_task.duration()
@@ -275,7 +286,16 @@ class CycleWindow(SiriusMainWindow):
 
         self._is_preparing = ''
         self.cycle_bt.setEnabled(False)
-        cycle_task = Cycle(pwrsupplies, self._timing, self)
+
+        create_task = CreateCyclers(parent=self, psnames=pwrsupplies)
+        dlg = ProgressDialog('Creating cycles...', create_task, self)
+        ret = dlg.exec_()
+        if ret == dlg.Rejected:
+            self._allButtons_setEnabled(True)
+            return
+
+        cycle_task = Cycle(parent=self, psnames=pwrsupplies,
+                           timing=self._timing)
         cycle_task.updated.connect(self._update_progress)
         duration = cycle_task.duration()
         self.progress_bar.setMinimum(0)
@@ -297,8 +317,12 @@ class CycleWindow(SiriusMainWindow):
         if not pwrsupplies:
             QMessageBox.critical(self, 'Message', 'No power supply selected!')
             return False
-        task = ZeroPSCurrent(pwrsupplies, self)
-        dlg = ProgressDialog('Setting currents to zero...', task, self)
+
+        task0 = CreateCyclers(parent=self, psnames=pwrsupplies)
+        task1 = ZeroPSCurrent(parent=self, psnames=pwrsupplies)
+        labels = ['Creating cycles...', 'Setting currents to zero...']
+        tasks = [task0, task1]
+        dlg = ProgressDialog(labels, tasks, self)
         ret = dlg.exec_()
         if ret == dlg.Rejected:
             return False
@@ -310,8 +334,12 @@ class CycleWindow(SiriusMainWindow):
         if not pwrsupplies:
             QMessageBox.critical(self, 'Message', 'No power supply selected!')
             return False
-        task = ResetPSOpMode(pwrsupplies, self)
-        dlg = ProgressDialog('Setting OpMode to SlowRef...', task, self)
+
+        task0 = CreateCyclers(parent=self, psnames=pwrsupplies)
+        task1 = ResetPSOpMode(parent=self, psnames=pwrsupplies)
+        labels = ['Creating cycles...', 'Setting OpMode to SlowRef...']
+        tasks = [task0, task1]
+        dlg = ProgressDialog(labels, tasks, self)
         ret = dlg.exec_()
         if ret == dlg.Rejected:
             return False
@@ -319,7 +347,7 @@ class CycleWindow(SiriusMainWindow):
     def _restore_timing(self):
         if not self._check_connected('timing'):
             return
-        task = RestoreTiming(self._timing, self)
+        task = RestoreTiming(parent=self, timing=self._timing)
         dlg = ProgressDialog('Restoring timing initial state...', task, self)
         ret = dlg.exec_()
         if ret == dlg.Rejected:
@@ -335,8 +363,6 @@ class CycleWindow(SiriusMainWindow):
         if without_linac:
             pwrsupplies = [ps for ps in pwrsupplies if 'LI' not in ps]
 
-        # Create new cyclers if needed
-        self._create_cyclers(pwrsupplies)
         return pwrsupplies
 
     def _get_sections(self):
@@ -346,19 +372,6 @@ class CycleWindow(SiriusMainWindow):
             if Filter.process_filters(ps2cycle, filters={'sec': s}):
                 sections.append(s)
         return sections
-
-    def _create_cyclers(self, psnames):
-        """Create new cyclers, if necessary."""
-        ps2create = list()
-        for name in psnames:
-            if name not in _cyclers.keys():
-                ps2create.append(name)
-        if not ps2create:
-            return
-
-        task = CreateCyclers(ps2create)
-        dlg = ProgressDialog('Connecting to power supplies...', task, self)
-        dlg.exec_()
 
     def _open_ps_detail(self, item):
         app = QApplication.instance()
@@ -503,355 +516,6 @@ class UpdateProgressBar(QThread):
                 break
             _time.sleep(1)
             self.increment.emit()
-
-
-# Tasks
-
-class CreateCyclers(QThread):
-    """Create cyclers."""
-
-    currentItem = Signal(str)
-    itemDone = Signal()
-    completed = Signal()
-
-    def __init__(self, psnames, parent=None):
-        super().__init__(parent)
-        self._psnames = psnames
-        self._quit_task = False
-
-    def size(self):
-        """Return task size."""
-        return len(self._psnames)
-
-    def exit_task(self):
-        """Set flag to quit thread."""
-        self._quit_task = True
-
-    def run(self):
-        """Create cyclers."""
-        if self._quit_task:
-            pass
-        else:
-            interrupted = False
-            threads = dict()
-            for psname in self._psnames:
-                threads[psname] = _Thread(
-                    target=self.create_cycler,
-                    args=(psname, ), daemon=True)
-                threads[psname].start()
-                if self._quit_task:
-                    interrupted = True
-                    break
-            for t in threads.values():
-                t.join()
-            if not interrupted:
-                self.completed.emit()
-
-    def create_cycler(self, psname):
-        global _cyclers
-        if psname in _cyclers.keys():
-            pass
-        else:
-            if PVName(psname).sec == 'LI':
-                c = LinacPSCycler(psname)
-            else:
-                c = PSCycler(psname)
-            with _lock:
-                _cyclers[psname] = c
-        self.currentItem.emit(psname)
-        self.itemDone.emit()
-
-
-class VerifyPS(QThread):
-    """Verify power supply initial state."""
-
-    currentItem = Signal(str)
-    itemDone = Signal(str, bool)
-    completed = Signal()
-
-    def __init__(self, psnames, parent=None):
-        """Constructor."""
-        super().__init__(parent)
-        self._psnames = psnames
-        self._quit_task = False
-
-    def size(self):
-        """Return task size."""
-        return len(self._psnames)
-
-    def exit_task(self):
-        """Set flag to quit thread."""
-        self._quit_task = True
-
-    def run(self):
-        """Set power supplies to cycling."""
-        if self._quit_task:
-            pass
-        else:
-            interrupted = False
-            threads = dict()
-            for psname in self._psnames:
-                threads[psname] = _Thread(
-                    target=self.check_pwrsupply, args=(psname, ), daemon=True)
-                threads[psname].start()
-                if self._quit_task:
-                    interrupted = True
-                    break
-            for t in threads.values():
-                if interrupted:
-                    t.stop()
-                t.join()
-            if not interrupted:
-                self.completed.emit()
-
-    def check_pwrsupply(self, psname):
-        global _cyclers
-        cycler = _cyclers[psname]
-        status = cycler.connected
-        if not status:
-            print(psname, 'connection')
-        status &= cycler.check_on()
-        if not status:
-            print(psname, 'off')
-        status &= cycler.check_intlks()
-        if not status:
-            print(psname, 'intlk')
-        self.currentItem.emit(psname)
-        self.itemDone.emit(psname, status)
-
-
-class ZeroPSCurrent(QThread):
-    """Set power supply current to zero."""
-
-    currentItem = Signal(str)
-    itemDone = Signal(str, bool)
-    completed = Signal()
-
-    def __init__(self, psnames, parent=None):
-        """Constructor."""
-        super().__init__(parent)
-        self._psnames = psnames
-        self._quit_task = False
-
-    def size(self):
-        """Return task size."""
-        return len(self._psnames)
-
-    def exit_task(self):
-        """Set flag to quit thread."""
-        self._quit_task = True
-
-    def run(self):
-        """Set power supplies current to zero."""
-        if self._quit_task:
-            pass
-        else:
-            interrupted = False
-            threads = dict()
-            for psname in self._psnames:
-                threads[psname] = _Thread(
-                    target=self.set_pwrsupply_current_zero,
-                    args=(psname, ), daemon=True)
-                threads[psname].start()
-                if self._quit_task:
-                    interrupted = True
-                    break
-            for t in threads.values():
-                t.join()
-            if not interrupted:
-                self.completed.emit()
-
-    def set_pwrsupply_current_zero(self, psname):
-        global _cyclers
-        done = _cyclers[psname].set_current_zero()
-        self.currentItem.emit(psname)
-        self.itemDone.emit(psname, done)
-
-
-class ResetPSOpMode(QThread):
-    """Set power supply to SlowRef."""
-
-    currentItem = Signal(str)
-    itemDone = Signal(str, bool)
-    completed = Signal()
-
-    def __init__(self, psnames, parent=None):
-        """Constructor."""
-        super().__init__(parent)
-        self._psnames = psnames
-        self._quit_task = False
-
-    def size(self):
-        """Return task size."""
-        return len(self._psnames)
-
-    def exit_task(self):
-        """Set flag to quit thread."""
-        self._quit_task = True
-
-    def run(self):
-        """Set power supplies to cycling."""
-        if self._quit_task:
-            pass
-        else:
-            interrupted = False
-            threads = dict()
-            for psname in self._psnames:
-                threads[psname] = _Thread(
-                    target=self.set_pwrsupply_opmode_slowref,
-                    args=(psname, ), daemon=True)
-                threads[psname].start()
-                if self._quit_task:
-                    interrupted = True
-                    break
-            for t in threads.values():
-                t.join()
-            if not interrupted:
-                self.completed.emit()
-
-    def set_pwrsupply_opmode_slowref(self, psname):
-        global _cyclers
-        done = _cyclers[psname].set_opmode_slowref()
-        self.currentItem.emit(psname)
-        self.itemDone.emit(psname, done)
-
-
-class PreparePS(QThread):
-    """Prepare power suplies to cycle."""
-
-    updated = Signal(str, bool, bool, bool)
-
-    def __init__(self, psnames, timing, ppty, parent=None):
-        super().__init__(parent)
-        cyclers = dict()
-        for ps in psnames:
-            cyclers[ps] = _cyclers[ps]
-        self._controller = CycleController(
-            cyclers=cyclers, timing=timing, logger=self)
-        self._ppty = ppty
-        self._quit_thread = False
-
-    def size(self):
-        return self._controller.prepare_ps_size
-
-    def duration(self):
-        """Return task maximum duration."""
-        return self._controller.prepare_ps_max_duration
-
-    def exit_thread(self):
-        self._quit_thread = True
-
-    def run(self):
-        if not self._quit_thread:
-            if self._ppty == 'params':
-                self._controller.prepare_pwrsupplies_parameters()
-            else:
-                self._controller.prepare_pwrsupplies_opmode()
-            self._quit_thread = True
-
-    def update(self, message, done, warning, error):
-        now = _datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
-        self.updated.emit(now+'  '+message, done, warning, error)
-
-
-class PrepareTiming(QThread):
-    """Prepare timing to cycle."""
-
-    updated = Signal(str, bool, bool, bool)
-
-    def __init__(self, psnames, timing, parent=None):
-        super().__init__(parent)
-        cyclers = dict()
-        for ps in psnames:
-            cyclers[ps] = _cyclers[ps]
-        self._controller = CycleController(
-            cyclers=cyclers, timing=timing, logger=self)
-        self._quit_thread = False
-
-    def size(self):
-        return self._controller.prepare_timing_size
-
-    def duration(self):
-        """Return task maximum duration."""
-        return self._controller.prepare_timing_max_duration
-
-    def exit_thread(self):
-        self._quit_thread = True
-
-    def run(self):
-        if not self._quit_thread:
-            self._controller.prepare_timing()
-            self._quit_thread = True
-
-    def update(self, message, done, warning, error):
-        now = _datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
-        self.updated.emit(now+'  '+message, done, warning, error)
-
-
-class RestoreTiming(QThread):
-    """Restore timing initial state."""
-
-    currentItem = Signal(str)
-    itemDone = Signal()
-    completed = Signal()
-
-    def __init__(self, timing, parent=None):
-        """Constructor."""
-        super().__init__(parent)
-        self._quit_task = False
-        self._timing = timing
-
-    def size(self):
-        """Return task size."""
-        return 2
-
-    def exit_task(self):
-        """Set flag to quit thread."""
-        self._quit_task = True
-
-    def run(self):
-        """Set power supplies to cycling."""
-        if self._quit_task:
-            pass
-        else:
-            self.itemDone.emit()
-            self._timing.restore_initial_state()
-            self.itemDone.emit()
-            self.completed.emit()
-
-
-class Cycle(QThread):
-    """Cycle."""
-
-    updated = Signal(str, bool, bool, bool)
-
-    def __init__(self, psnames, timing, parent=None):
-        super().__init__(parent)
-        cyclers = dict()
-        for ps in psnames:
-            cyclers[ps] = _cyclers[ps]
-        self._controller = CycleController(
-            cyclers=cyclers, timing=timing, logger=self)
-        self._quit_thread = False
-
-    def size(self):
-        return self._controller.cycle_size
-
-    def duration(self):
-        """Return task maximum duration."""
-        return self._controller.cycle_max_duration
-
-    def exit_thread(self):
-        self._quit_thread = True
-
-    def run(self):
-        if not self._quit_thread:
-            self._controller.cycle()
-            self._quit_thread = True
-
-    def update(self, message, done, warning, error):
-        now = _datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
-        self.updated.emit(now+'  '+message, done, warning, error)
 
 
 if __name__ == '__main__':
