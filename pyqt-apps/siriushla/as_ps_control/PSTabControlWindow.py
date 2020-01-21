313274d730381a75386f5a33725f631a1fdddf3b
anacso17 <anacso1777@gmail.com>
2020-01-21 12:26:26 -0300

    ENH: improve PSControl windows  - Decrease window creation interval - Add option to show PS BBB and UDC - Change searchbar to search within PS, DCLinks, BBB and UDC names.

--- pyqt-apps/siriushla/as_ps_control/PSTabControlWindow.py
+++ pyqt-apps/siriushla/as_ps_control/PSTabControlWindow.py
@@ -1,6 +1,8 @@
 """Defines a class to control elements from a given class."""
-from qtpy.QtWidgets import QTabWidget, QWidget, QGridLayout
-
+import time as _time
+from qtpy.QtWidgets import QTabWidget, QWidget, QGridLayout, QLabel
+from qtpy.QtCore import Signal, QThread
+from siriushla.widgets import SiriusDialog
 from .PSControlWindow import PSControlWindow
 from .control_widget.ControlWidgetFactory import ControlWidgetFactory
 
@@ -24,6 +26,8 @@ class PSTabControlWindow(PSControlWindow):
                "trim-quadrupole": "Trims",
                }
 
+    tabFilled = Signal()
+
     def __init__(self, section, parent=None):
         """Class constructor."""
         super().__init__(section=section,
@@ -35,16 +39,43 @@ class PSTabControlWindow(PSControlWindow):
         # Create Tabs
         self.tabs = QTabWidget()
         self.tabs.setObjectName(self._section + "Tab")
+        self.TabIndex = dict()
         self._addTabs()
+        self.tabs.currentChanged.connect(self._fillTabTask)
         # Set widget layout
         self.setCentralWidget(self.tabs)
 
     def _addTabs(self):
         for device in self.Devices[self._section]:
-            widget = ControlWidgetFactory.factory(
-                self, self._section, device)
-            self._connect_buttons(widget)
-            self.tabs.addTab(widget, self.TabName[device])
+            widget = QWidget(self)
+            index = self.tabs.addTab(widget, self.TabName[device])
+            self.TabIndex[index] = {
+                'device': device, 'widget': widget, 'filled': False}
+            if index in range(3):
+                self._fillTab(index)
+
+    def _fillTabTask(self, index):
+        if self.TabIndex[index]['filled']:
+            return
+        th = _WaitThread(parent=self, index=index)
+        dlg = _WaitDialog(self)
+        th.opendiag.connect(dlg.show)
+        th.closediag.connect(dlg.close)
+        th.tabIndex.connect(self._fillTab)
+        self.tabFilled.connect(th.exit_thread)
+        th.start()
+
+    def _fillTab(self, index):
+        self.TabIndex[index]['filled'] = True
+        device = self.TabIndex[index]['device']
+        widget = self.TabIndex[index]['widget']
+        widget.setLayout(QGridLayout())
+        widget.layout().setContentsMargins(0, 0, 0, 0)
+        tabwid = ControlWidgetFactory.factory(
+            self, self._section, device)
+        self._connect_buttons(tabwid)
+        widget.layout().addWidget(tabwid)
+        self.tabFilled.emit()
 
     def _dipoleWidgetWrapper(self, widget):
         wrapper = QWidget(self)
@@ -56,3 +87,35 @@ class PSTabControlWindow(PSControlWindow):
         wrapper.layout.setColumnStretch(1, 1)
 
         return wrapper
+
+
+class _WaitDialog(SiriusDialog):
+    """Auxiliary dialog to show during a long task."""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Wait')
+        lay = QGridLayout(self)
+        lay.addWidget(QLabel('Wait a moment...'))
+
+
+class _WaitThread(QThread):
+
+    opendiag = Signal()
+    closediag = Signal()
+    tabIndex = Signal(int)
+
+    def __init__(self, parent=None, section='', index={}):
+        super().__init__(parent)
+        self._index = index
+        self._exit_thread = False
+
+    def exit_thread(self):
+        self._exit_thread = True
+
+    def run(self):
+        self.opendiag.emit()
+        self.tabIndex.emit(self._index)
+        while not self._exit_thread:
+            _time.sleep(0.1)
+        self.closediag.emit()
