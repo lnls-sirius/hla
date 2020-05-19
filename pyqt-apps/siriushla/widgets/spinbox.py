lnls-sirius/hla

commit cf8b90ac4b6093ef6cf1a78c41697cd28016f37c

WID.BUG: Bypass bugged `precision_changed` method from `PyDMSpinbox`.

pyqt-apps/siriushla/widgets/spinbox.py
@@ -1,6 +1,6 @@
 from qtpy.QtCore import Property
 from pydm.widgets import PyDMSpinbox
-from pydm.widgets.base import PyDMWritableWidget
+from pydm.widgets.base import PyDMWritableWidget, TextFormatter
 
 
 class SiriusSpinbox(PyDMSpinbox):
@@ -66,7 +66,7 @@ class SiriusSpinbox(PyDMSpinbox):
         new_precison : int or float
             The new precision value
         """
-        super(SiriusSpinbox, self).precision_changed(new_precision)
+        TextFormatter.precision_changed(self, new_precision)
         self.update_limits()
 
     @Property(bool)

commit fe010e7015da72c5a9736cb145936f3ca60443fc

WID.BUG: fix bug introduced in cf8b90ac4.
need to call `setDecimals` since `PyDMSpinbox.precision_changed` is not being called anymore.

pyqt-apps/siriushla/widgets/spinbox.py
@@ -67,6 +67,7 @@ class SiriusSpinbox(PyDMSpinbox):
             The new precision value
         """
         TextFormatter.precision_changed(self, new_precision)
+        self.setDecimals(self.precision)
         self.update_limits()
 
     @Property(bool)