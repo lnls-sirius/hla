0f7ae3ee1d0c57e662287294f976048d7fa3bc07
anacso17 <anacso1777@gmail.com>
2020-06-01 15:28:02 -0300

    PS.ENH: improve __init__.py file

--- pyqt-apps/siriushla/as_ps_control/__init__.py
+++ pyqt-apps/siriushla/as_ps_control/__init__.py
@@ -6,3 +6,6 @@ from .PSTabControlWindow import PSTabControlWindow
 from .PSTrimWindow import PSTrimWindow
 from .SummaryWidgets import SummaryWidget, SummaryHeader
 from .ps_wfmerror_mon import PlotWfmErrorWindow
+
+from .control_widget.ControlWidgetFactory import ControlWidgetFactory
+from .detail_widget.DetailWidgetFactory import DetailWidgetFactory
