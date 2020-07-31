d5f25acce11e4da4813fedfb0ae560e1e3ef4876
Fernando <fernandohds564@gmail.com>
2020-07-30 22:16:53 -0300

    SOFB.BUG: Fix reset PID button.

--- pyqt-apps/siriushla/as_ap_sofb/ioc_control/main.py
+++ pyqt-apps/siriushla/as_ap_sofb/ioc_control/main.py
@@ -278,6 +278,7 @@ class SOFBControl(BaseWidget):
         hbl.addWidget(QLabel('Reset PID ref'))
         wid = PyDMPushButton(
             gpbx, init_channel=self.prefix+'LoopPIDRstRef-Cmd')
+        wid.pressValue = 1
         wid.setToolTip('Reset PID reference')
         wid.setIcon(qta.icon('fa5s.sync'))
         hbl.addWidget(wid)
