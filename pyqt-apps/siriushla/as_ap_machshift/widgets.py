6d087176238fef43f260b653a02942823ad9c8cd
anacso17 <anacso1777@gmail.com>
2021-08-23 14:26:01 -0300

    ENH: add MachShiftLabel widget

---
+++ pyqt-apps/siriushla/as_ap_machshift/widgets.py
@@ -0,0 +1,37 @@
+"""MachShift widgets."""
+
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QWidget, QGridLayout
+
+from ..widgets import SiriusLabel, SiriusFrame
+
+
+class MachShiftLabel(QWidget):
+    """Machine Shift Label."""
+
+    def __init__(self, parent=None, prefix=''):
+        """Init."""
+        super().__init__(parent)
+
+        self.label = SiriusLabel(
+            self, prefix+'AS-Glob:AP-MachShift:Mode-Sts')
+        self.label.setAlignment(Qt.AlignCenter)
+
+        color_list = [
+            SiriusFrame.Yellow,  # Users
+            SiriusFrame.LightBlue,  # Commissioning
+            SiriusFrame.DarkCyan,  # Conditioning
+            SiriusFrame.LightSalmon,  # Injection
+            SiriusFrame.LightBlue,  # MachineStudy
+            SiriusFrame.LightGreen,  # Maintenance
+            SiriusFrame.Salmon,  # Standby
+            SiriusFrame.Lavender,  # Shutdown
+        ]
+        self.frame = SiriusFrame(
+            self, prefix+'AS-Glob:AP-MachShift:Mode-Sts',
+            color_list=color_list, is_float=False)
+        self.frame.add_widget(self.label)
+
+        lay = QGridLayout(self)
+        lay.setContentsMargins(0, 0, 0, 0)
+        lay.addWidget(self.frame)
