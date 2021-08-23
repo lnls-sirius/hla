9f1922ef0dcc40113c4bcbdb966b3290adae6557
anacso17 <anacso1777@gmail.com>
2021-08-23 14:59:37 -0300

    TI.ENH: add timing specialized widgets

--- pyqt-apps/siriushla/as_ti_control/__init__.py
+++ pyqt-apps/siriushla/as_ti_control/__init__.py
@@ -3,3 +3,5 @@ from .monitor import MonitorWindow
 from .low_level_devices import EVG, EventList, ClockList, BucketList, \
     FOUT, AFC, EVR, EVE, LLTriggerList
 from .hl_trigger import HLTriggerDetailed, HLTriggerSimple, HLTriggerList
+from .widgets import EVGContinuousButton, EVGContinuousLed, \
+    EVGInjectionButton, EVGInjectionLed, EVGUpdateEvtButton, EVGUpdateEvtLed
