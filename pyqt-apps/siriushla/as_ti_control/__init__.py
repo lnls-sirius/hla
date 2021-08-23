6cdb27eeb07c7999f2b0980015712aa3164801aa
anacso17 <anacso1777@gmail.com>
2021-08-23 15:05:40 -0300

    TI.ENH: add BucketListGraph widget

--- pyqt-apps/siriushla/as_ti_control/__init__.py
+++ pyqt-apps/siriushla/as_ti_control/__init__.py
@@ -1,7 +1,8 @@
 from .hltiming import TimingMain
 from .monitor import MonitorWindow
 from .low_level_devices import EVG, EventList, ClockList, BucketList, \
-    FOUT, AFC, EVR, EVE, LLTriggerList
+    FOUT, AFC, EVR, EVE, LLTriggerList, BucketListLabel, BucketListLineEdit, \
+    BucketListGraph
 from .hl_trigger import HLTriggerDetailed, HLTriggerSimple, HLTriggerList
 from .widgets import EVGContinuousButton, EVGContinuousLed, \
     EVGInjectionButton, EVGInjectionLed, EVGUpdateEvtButton, EVGUpdateEvtLed
