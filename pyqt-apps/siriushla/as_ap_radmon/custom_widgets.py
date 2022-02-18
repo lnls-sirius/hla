d117ee970ccb6debe5c90048d51edd1c12859a8c
anacso17 <ana.clara@lnls.br>
2022-02-18 12:33:43 -0300

    Rad.ENH: implement @fernandohds564 suggestions:
    - use mouseMoveEvent instead of mousePressEvent to show tooltip
    - improve nearest point search
    - use range relative threshold to show tooltip
    - plot an scatter in nearest point

--- pyqt-apps/siriushla/as_ap_radmon/custom_widgets.py
+++ pyqt-apps/siriushla/as_ap_radmon/custom_widgets.py
@@ -6,6 +6,8 @@ from qtpy.QtCore import Qt
 from qtpy.QtGui import QPalette
 from qtpy.QtWidgets import QToolTip
 
+from pyqtgraph import mkBrush
+
 from siriuspy.clientarch import Time as _Time
 
 from siriushla.sirius_application import SiriusApplication
@@ -13,40 +15,47 @@ from siriushla.widgets import SiriusTimePlot
 
 
 class MyTimePlot(SiriusTimePlot):
-    """Reimplement mousePressEvent."""
+    """Reimplement mouseMoveEvent."""
 
-    def mousePressEvent(self, ev):
-        if ev.button() == Qt.LeftButton:
-            pos = ev.pos()
+    def mouseMoveEvent(self, ev):
+        pos = ev.pos()
 
-            # find nearest curve point
-            nearest = (self._curves[0], _np.inf, 0)
-            for idx, curve in enumerate(self._curves):
-                if not curve.isVisible():
-                    continue
-                mappos = curve.mapFromScene(pos)
-                posx, posy = mappos.x(), mappos.y()
-                xData, yData = curve.curve.xData, curve.curve.yData
-                idx = _np.where(_np.isclose(xData-posx, 0, atol=0.5))[0]
-                if idx.size:
-                    valx, valy = xData[idx[0]], yData[idx[0]]
-                    diff = abs(valy - posy)
-                    if diff < nearest[1]:
-                        nearest = (curve, diff, valy)
+        # find nearest curve point
+        nearest = (self._curves[0], _np.inf, None, None)
+        for idx, curve in enumerate(self._curves):
+            if not curve.isVisible():
+                continue
+            mappos = curve.mapFromScene(pos)
+            posx, posy = mappos.x(), mappos.y()
+            xData, yData = curve.curve.xData, curve.curve.yData
+            if not xData.size:
+                continue
+            idx = _np.argmin(_np.abs(xData-posx))
+            if idx.size:
+                valx, valy = xData[idx], yData[idx]
+                diff = abs(valy - posy)
+                if diff < nearest[1]:
+                    nearest = (curve, diff, valx, valy)
 
-            # show tooltip
-            curve, diff, valy = nearest
-            if diff < 0.01:
-                txt = _Time(timestamp=valx).get_iso8601()+'\n'
-                txt += f'{curve.name()}: {valy:.3f}'
-                font = SiriusApplication.instance().font()
-                font.setPointSize(font.pointSize() - 10)
-                QToolTip.setFont(font)
-                palette = QPalette()
-                palette.setColor(QPalette.ToolTipText, curve.color)
-                palette.setColor(QPalette.ToolTipBase, Qt.darkGray)
-                QToolTip.setPalette(palette)
-                QToolTip.showText(
-                    self.mapToGlobal(pos), txt, self, self.geometry(), 5000)
-        else:
-            super().mousePressEvent(ev)
+        # show tooltip
+        curve, diff, valx, valy = nearest
+        ylimts = self.getViewBox().state['viewRange'][1]
+        ydelta = ylimts[1] - ylimts[0]
+        if diff < 2e-2*ydelta:
+            txt = _Time(timestamp=valx).get_iso8601()+'\n'
+            txt += f'{curve.name()}: {valy:.3f}'
+            font = SiriusApplication.instance().font()
+            font.setPointSize(font.pointSize() - 10)
+            QToolTip.setFont(font)
+            palette = QPalette()
+            palette.setColor(QPalette.ToolTipText, curve.color)
+            palette.setColor(QPalette.ToolTipBase, Qt.darkGray)
+            QToolTip.setPalette(palette)
+            QToolTip.showText(
+                self.mapToGlobal(pos), txt, self, self.geometry(), 1000)
+            curve.scatter.setData(
+                pos=[(valx, valy), ], symbol='o', size=15,
+                brush=mkBrush(curve.color))
+            curve.scatter.show()
+
+        super().mouseMoveEvent(ev)
