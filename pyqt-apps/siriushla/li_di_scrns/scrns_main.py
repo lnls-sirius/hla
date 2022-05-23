82088f25cbc6be573b437c621e1ae40183c1d4ea
rafaellyra8 <rafaellyra2010@hotmail.com>
2022-05-23 17:04:09 -0300

    WIP - Image

--- pyqt-apps/siriushla/li_di_scrns/scrns_main.py
+++ pyqt-apps/siriushla/li_di_scrns/scrns_main.py
@@ -1,12 +1,16 @@
-''' Diagnostic Interface of the LINAC's BPM'''
+''' Diagnostic Interface of the LINAC's Screen'''
+import os as _os
 from qtpy.QtCore import Qt
-from qtpy.QtGui import QPixmap
-from qtpy.QtWidgets import QGroupBox, QHBoxLayout, QVBoxLayout, \
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets  import QGroupBox, QHBoxLayout, QVBoxLayout, \
     QWidget, QLabel, QGridLayout
+from pydm.widgets import PyDMEnumComboBox, PyDMLabel
 import qtawesome as qta
 from ..util import get_appropriate_color
 from ..widgets import SiriusMainWindow
 
+#mdi-safe-square
+#mdi-camera-metering-center
 
 class LiBeamProfile(SiriusMainWindow):
     ''' Linac Profile Screen '''
@@ -19,11 +23,29 @@ class LiBeamProfile(SiriusMainWindow):
         self.setObjectName('LIApp')
         color = get_appropriate_color('LI')
 
-        self.device_name = "LA-BI:"
+        self.device = 'PRF2:'
+        self.device_name = 'LA-BI:' + self.device
+
         self.setWindowIcon(qta.icon('mdi.safe-square', color=color))
         self.setWindowTitle(self.device_name)
+        self.image_container = QLabel()
+        self.pixmap = QPixmap(_os.path.join(
+                _os.path.abspath(_os.path.dirname(__file__)), "linac.jpg"))
+
 
         self._setupUi()
+
+    def resizeEvent(self, event):
+        self.image_container.setMinimumSize(1, 1)
+        self.image_container.setPixmap(
+            self.pixmap.scaled(
+                self.image_container.width(),
+                self.image_container.height())
+        )
+        SiriusMainWindow.resizeEvent(self, event)
+
+    def getPvName(self, pv_name):
+        return self.device_name + "MOTOR:" + pv_name
 
     def header(self):
         hd_hlay = QHBoxLayout()
@@ -33,10 +55,15 @@ class LiBeamProfile(SiriusMainWindow):
         return hd_hlay
 
     def imageViewer(self):
-        pixmap = QPixmap('./linac.jpg')
-        image_container = QLabel()
-        image_container.setPixmap(pixmap)
-        return image_container
+        img_hlay = QHBoxLayout()
+        self.image_container.resize(650, 250)
+        self.image_container.setPixmap(
+            self.pixmap.scaled(
+                self.image_container.width(),
+                self.image_container.height())
+        )
+        img_hlay.addWidget(self.image_container)
+        return img_hlay
 
     def _setupUi(self):
         '''Build the graphic interface'''
@@ -44,13 +71,8 @@ class LiBeamProfile(SiriusMainWindow):
         if_glay = QGridLayout()
 
         if_glay.addLayout(self.header(), 0, 0, 1, 1)
-        if_glay.addWidget(self.imageViewer(), 1, 0, 1, 1)
-        if_glay.setAlignment(Qt.AlignTop)
+        if_glay.addLayout(self.imageViewer(), 1, 0, 50, 1)
+        # if_glay.addWidget(PyDMEnumComboBox(self, init_channel=self.getPvName('POS_R')), 0, 1, 1, 1)
+        # if_glay.addWidget(PyDMLabel(self, init_channel=self.getPvName('BUSY')), 1, 1, 1, 1)
         wid.setLayout(if_glay)
         self.setCentralWidget(wid)
-
-# mdi-circle-double
-#mdi-record-circle-outline
-#mdi-cash
-#mdi-safe-square-outline
-#mdi-safe-square
