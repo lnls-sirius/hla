00b03a62a0758f86b7c1cb736fcf63dc43211609
Ana Clara Oliveira <ana.clara@lnls.br>
2022-07-13 12:54:37 -0300

    MNT: don't list empty channels in channels method once now tooltip lists all widget channels

--- pyqt-apps/siriushla/widgets/spectrogram_view.py
+++ pyqt-apps/siriushla/widgets/spectrogram_view.py
@@ -1434,7 +1434,7 @@ class SiriusSpectrogramView(
         channels : list
             List of PyDMChannel objects
         """
-        return self._channels
+        return [ch for ch in self._channels if ch is not None]
 
     def channels_for_tools(self):
         """Return channels for tools."""
