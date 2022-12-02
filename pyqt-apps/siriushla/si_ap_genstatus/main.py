7fae5ee835f4a1342c305569048eea8ea6364982
Ana Clara Oliveira <ana.clara@lnls.br>
2022-12-02 14:26:13 -0300

    GenSts.ENH: add InjMode and show top-up settings when in top-up mode

--- pyqt-apps/siriushla/si_ap_genstatus/main.py
+++ pyqt-apps/siriushla/si_ap_genstatus/main.py
@@ -12,6 +12,7 @@ import qtawesome as qta
 from siriuspy.envars import VACA_PREFIX
 from siriuspy.namesys import SiriusPVName as _PVName
 from siriuspy.clientarch.time import Time
+from siriuspy.injctrl.csdev import Const as _InjConst
 
 from ..util import get_appropriate_color
 from ..widgets import SiriusMainWindow, SiriusTimePlot, SiriusFrame, \
@@ -37,17 +38,91 @@ class SIGenStatusWindow(SiriusMainWindow):
         self._setupUi()
 
     def _setupUi(self):
-        # controls
+        # machine shift
         self.ld_machsht = QLabel(
             '<h3>Shift: </h3>', self, alignment=Qt.AlignCenter)
         self.lb_machsht = MachShiftLabel(self, prefix=self.prefix)
         self.lb_machsht.label.setStyleSheet('QLabel{font-size: 45pt;}')
-        box_mach = QHBoxLayout()
-        box_mach.addWidget(self.ld_machsht)
-        box_mach.addWidget(self.lb_machsht)
-        box_mach.setStretch(0, 1)
-        box_mach.setStretch(1, 4)
 
+        # injection mode
+        self.ld_injmode = QLabel(
+            '<h3>Inj.Mode: </h3>', self, alignment=Qt.AlignCenter)
+        injctrl_dev = _PVName('AS-Glob:AP-InjCtrl')
+        injctrl_dev = injctrl_dev.substitute(prefix=self.prefix)
+        pvname = injctrl_dev.substitute(propty='Mode-Sts')
+        self.lb_injmode = SiriusLabel(self, pvname, alignment=Qt.AlignCenter)
+        self.lb_injmode.setStyleSheet('QLabel{font-size: 45pt;}')
+        topupwids = list()
+
+        self.ld_tunxt = QLabel(
+            '<h3>Next Inj.: </h3>', self, alignment=Qt.AlignCenter)
+        self.lb_tunxt = SiriusLabel(
+            self, injctrl_dev.substitute(propty='TopUpNextInj-Mon'))
+        self.lb_tunxt.displayFormat = SiriusLabel.DisplayFormat.Time
+        self.lb_tunxt.setAlignment(Qt.AlignCenter)
+        self.lb_tunxt.setStyleSheet('QLabel{font-size: 45pt;}')
+        box_tunxt = QWidget()
+        lay_tunxt = QVBoxLayout(box_tunxt)
+        lay_tunxt.setContentsMargins(0, 0, 0, 0)
+        lay_tunxt.addWidget(self.ld_tunxt)
+        lay_tunxt.addWidget(self.lb_tunxt)
+        topupwids.append(box_tunxt)
+
+        self.ld_tusts = QLabel(
+            '<h3>Status: </h3>', self, alignment=Qt.AlignCenter)
+        self.lb_tusts = SiriusLabel(
+            self, injctrl_dev.substitute(propty='TopUpState-Sts'))
+        self.lb_tusts.setAlignment(Qt.AlignCenter)
+        self.lb_tusts.setStyleSheet('QLabel{font-size: 45pt;}')
+        box_tusts = QWidget()
+        lay_tusts = QVBoxLayout(box_tusts)
+        lay_tusts.setContentsMargins(0, 0, 0, 0)
+        lay_tusts.addWidget(self.ld_tusts)
+        lay_tusts.addWidget(self.lb_tusts)
+        topupwids.append(box_tusts)
+
+        self.ld_tuperd = QLabel(
+            '<h3>Period: </h3>', self, alignment=Qt.AlignCenter)
+        self.lb_tuperd = SiriusLabel(
+            self, injctrl_dev.substitute(propty='TopUpPeriod-RB'),
+            keep_unit=True, alignment=Qt.AlignCenter)
+        self.lb_tuperd.showUnits = True
+        self.lb_tuperd.setStyleSheet('QLabel{font-size: 45pt;}')
+        box_tuperd = QWidget()
+        lay_tuperd = QVBoxLayout(box_tuperd)
+        lay_tuperd.setContentsMargins(0, 0, 0, 0)
+        lay_tuperd.addWidget(self.ld_tuperd)
+        lay_tuperd.addWidget(self.lb_tuperd)
+        topupwids.append(box_tuperd)
+
+        self.ld_tunrpu = QLabel(
+            '<h3>Nr.Pulses: </h3>', self, alignment=Qt.AlignCenter)
+        self.lb_tunrpu = SiriusLabel(
+            self, injctrl_dev.substitute(propty='TopUpNrPulses-RB'),
+            alignment=Qt.AlignCenter)
+        self.lb_tunrpu.showUnits = True
+        self.lb_tunrpu.setStyleSheet('QLabel{font-size: 45pt;}')
+        box_tunrpu = QWidget()
+        lay_tunrpu = QVBoxLayout(box_tunrpu)
+        lay_tunrpu.setContentsMargins(0, 0, 0, 0)
+        lay_tunrpu.addWidget(self.ld_tunrpu)
+        lay_tunrpu.addWidget(self.lb_tunrpu)
+        topupwids.append(box_tunrpu)
+
+        self._gbox_topup = self._create_groupbox(
+            'Top-up status', topupwids, orientation='H')
+        self._gbox_topup.setVisible(False)
+        self.ch_injmode = SiriusConnectionSignal(pvname)
+        self.ch_injmode.new_value_signal[int].connect(
+            self._handle_injmode_settings_vis)
+
+        box_macsts = QGridLayout()
+        box_macsts.addWidget(self.ld_machsht, 0, 0)
+        box_macsts.addWidget(self.lb_machsht, 1, 0)
+        box_macsts.addWidget(self.ld_injmode, 0, 1)
+        box_macsts.addWidget(self.lb_injmode, 1, 1)
+
+        # interlock
         pvname = _PVName('RA-RaSIA02:RF-IntlkCtrl:IntlkSirius-Mon')
         pvname = pvname.substitute(prefix=self.prefix)
         self._led_siriusintlk = SiriusLedAlert(self, pvname)
@@ -56,12 +131,14 @@ class SIGenStatusWindow(SiriusMainWindow):
         self._gbox_siriusintlk = self._create_groupbox(
             'Sirius Interlock', self._led_siriusintlk)
 
+        # injection system
         self._led_injsyssts = InjSysStbyLed(self, self.prefix)
         self._led_injsyssts.setStyleSheet(
             'QLed{min-width:3em;min-height:3em;max-width:3em;max-height:3em;}')
         self._gbox_injsyssts = self._create_groupbox(
             'Injection System', self._led_injsyssts)
 
+        # SI SOFB
         pvname = _PVName('SI-Glob:AP-SOFB:LoopState-Sts')
         pvname = pvname.substitute(prefix=self.prefix)
         self._led_sofbloop = SiriusLedState(self, pvname)
@@ -71,6 +148,7 @@ class SIGenStatusWindow(SiriusMainWindow):
         self._gbox_sofbloop = self._create_groupbox(
             'SOFB', self._led_sofbloop)
 
+        # FOFB
         pvname = _PVName('SI-Glob:AP-FOFB:LoopState-Sts')
         pvname = pvname.substitute(prefix=self.prefix)
         self._led_fofbloop = SiriusLedState(self, pvname)
@@ -80,6 +158,7 @@ class SIGenStatusWindow(SiriusMainWindow):
         self._gbox_fofbloop = self._create_groupbox(
             'FOFB', self._led_fofbloop)
 
+        # BbB
         ledsbbb = list()
         for axis in ['H', 'V', 'L']:
             bbb_pref = _PVName('SI-Glob:DI-BbBProc-'+axis)
@@ -125,6 +204,7 @@ class SIGenStatusWindow(SiriusMainWindow):
         lay_curr.addWidget(self.ld_curr)
         lay_curr.addWidget(self.frm_curr)
 
+        # lifetime
         self.ld_lifetime = QLabel(
             '<h3>Lifetime</h3>', self, alignment=Qt.AlignCenter)
         self.ld_lifetime.setStyleSheet('max-height: 2em;')
@@ -141,11 +221,13 @@ class SIGenStatusWindow(SiriusMainWindow):
         lay_lt.addWidget(self.ld_lifetime)
         lay_lt.addWidget(self.lb_lifetime)
 
+        # tune
         self.tune_mon = SITuneMonitor(self, self.prefix, description='short',
                                       use_color_labels=False)
         self.tune_mon.lb_tunefrach.setStyleSheet('QLabel{font-size: 45pt;}')
         self.tune_mon.lb_tunefracv.setStyleSheet('QLabel{font-size: 45pt;}')
 
+        # current history
         self.curr_graph = SiriusTimePlot(self)
         self.curr_graph.setObjectName('curr_graph')
         self.curr_graph.setStyleSheet(
@@ -153,10 +235,10 @@ class SIGenStatusWindow(SiriusMainWindow):
         self.curr_graph.showXGrid = True
         self.curr_graph.showYGrid = True
         self.curr_graph.backgroundColor = QColor(255, 255, 255)
-        for ax in self.curr_graph.getPlotItem().axes.values():
-            sty = ax['item'].labelStyle
+        for axis in self.curr_graph.getPlotItem().axes.values():
+            sty = axis['item'].labelStyle
             sty['font-size'] = '18pt'
-            ax['item'].setLabel(text=None, **sty)
+            axis['item'].setLabel(text=None, **sty)
         self.curr_graph.setLabel('left', text='Current [mA]')
         font = QFont()
         font.setPointSize(18)
@@ -185,7 +267,6 @@ class SIGenStatusWindow(SiriusMainWindow):
             t_init=t_init.get_iso8601(), t_end=t_end.get_iso8601(),
             process_type='mean', process_bin_intvl=10)
 
-        cw = QWidget()
         hlay1 = QHBoxLayout()
         hlay1.setSpacing(30)
         hlay1.setAlignment(Qt.AlignVCenter)
@@ -204,22 +285,26 @@ class SIGenStatusWindow(SiriusMainWindow):
         hlay2.setStretch(1, 1)
         hlay2.setStretch(2, 1)
         hlay2.setStretch(3, 1)
+        hlay2.setStretch(4, 1)
 
-        lay = QGridLayout(cw)
+        cwid = QWidget()
+        lay = QGridLayout(cwid)
         lay.setVerticalSpacing(16)
         lay.setHorizontalSpacing(14)
-        lay.addLayout(box_mach, 0, 1)
+        lay.addLayout(box_macsts, 0, 0, 1, 3)
         lay.addLayout(hlay1, 1, 0, 1, 3)
         lay.addWidget(self.curr_graph, 2, 0, 1, 3)
         lay.addLayout(hlay2, 3, 0, 1, 3)
+        lay.addWidget(self._gbox_topup, 4, 0, 1, 3)
         lay.setRowStretch(0, 1)
-        lay.setRowStretch(1, 2)
-        lay.setRowStretch(2, 10)
-        lay.setRowStretch(3, 2)
+        lay.setRowStretch(1, 1)
+        lay.setRowStretch(2, 5)
+        lay.setRowStretch(3, 1)
+        lay.setRowStretch(4, 1)
         lay.setColumnStretch(0, 1)
         lay.setColumnStretch(1, 1)
         lay.setColumnStretch(2, 1)
-        self.setCentralWidget(cw)
+        self.setCentralWidget(cwid)
 
     @Slot(float)
     def formatLifetimeLabel(self, value):
@@ -239,41 +324,41 @@ class SIGenStatusWindow(SiriusMainWindow):
 
         lay = QVBoxLayout(gbox) if orientation == 'V' else QHBoxLayout(gbox)
         lay.setAlignment(Qt.AlignCenter)
-        lay.addStretch()
-        for wid in widgets:
+        for idx, wid in enumerate(widgets):
             lay.addWidget(wid)
-        lay.addStretch()
+            lay.setStretch(idx, 1)
         return gbox
 
+    def _handle_injmode_settings_vis(self, value):
+        self._gbox_topup.setVisible(value == _InjConst.InjMode.TopUp)
+
     def changeEvent(self, event):
+        """Change event handler."""
         if event.type() == QEvent.FontChange:
             fontsize = self.app.font().pointSize()
 
             # labels
-            self.lb_machsht.label.setStyleSheet(
-                'QLabel{font-size: '+str(fontsize+35)+'pt;}')
+            sty = 'QLabel{font-size: '+str(fontsize+35)+'pt;}'
+            self.lb_machsht.label.setStyleSheet(sty)
+            self.lb_injmode.setStyleSheet(sty)
             self.lb_curr.setStyleSheet(
                 'QLabel{background-color: '+self.app_color+';'
                 'font-size: '+str(fontsize+35)+'pt;}')
-            self.lb_lifetime.setStyleSheet(
-                'QLabel{font-size: '+str(fontsize+35)+'pt;}')
-            self.tune_mon.lb_tunefrach.setStyleSheet(
-                'QLabel{font-size: '+str(fontsize+35)+'pt;}')
-            self.tune_mon.lb_tunefracv.setStyleSheet(
-                'QLabel{font-size: '+str(fontsize+35)+'pt;}')
+            self.lb_lifetime.setStyleSheet(sty)
+            self.tune_mon.lb_tunefrach.setStyleSheet(sty)
+            self.tune_mon.lb_tunefracv.setStyleSheet(sty)
+            self.lb_tunxt.setStyleSheet(sty)
+            self.lb_tusts.setStyleSheet(sty)
+            self.lb_tuperd.setStyleSheet(sty)
+            self.lb_tunrpu.setStyleSheet(sty)
 
-            self._gbox_siriusintlk.setStyleSheet(
-                'QGroupBox{font-size: '+str(fontsize+2)+'pt;'
-                'font-weight: bold;}')
-            self._gbox_injsyssts.setStyleSheet(
-                'QGroupBox{font-size: '+str(fontsize+2)+'pt;'
-                'font-weight: bold;}')
-            self._gbox_sofbloop.setStyleSheet(
-                'QGroupBox{font-size: '+str(fontsize+2)+'pt;'
-                'font-weight: bold;}')
-            self._gbox_bbbloop.setStyleSheet(
-                'QGroupBox{font-size: '+str(fontsize+2)+'pt;'
-                'font-weight: bold;}')
+            sty = 'QGroupBox{font-size: '+str(fontsize+2)+'pt;'+\
+                'font-weight: bold;}'
+            self._gbox_siriusintlk.setStyleSheet(sty)
+            self._gbox_injsyssts.setStyleSheet(sty)
+            self._gbox_sofbloop.setStyleSheet(sty)
+            self._gbox_bbbloop.setStyleSheet(sty)
+            self._gbox_topup.setStyleSheet(sty)
 
             # graph
             graph_fontsize = fontsize + 2
