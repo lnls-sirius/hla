89ea4bc5ca7cc85fc9220bbc404f82bb7b4395ec
Ana Clara Oliveira <ana.clara@lnls.br>
2023-05-12 13:14:49 -0300

    ImgProc.FIX: fix datetime formating

--- pyqt-apps/siriushla/bl_ap_imgproc/main.py
+++ pyqt-apps/siriushla/bl_ap_imgproc/main.py
@@ -53,7 +53,7 @@ class BLImgProc(QWidget):
 
     def format_datetime_lbl(self, value, pvname):
         dtval = datetime.fromtimestamp(value)
-        datetime_lbl = dtval.strftime("%m/%d/%Y, %H:%M:%S")
+        datetime_lbl = dtval.strftime("%d/%m/%Y, %H:%M:%S")
         self._lbl_timestamp[pvname].setText(datetime_lbl)
 
     def create_time_widget(self, pvname):
