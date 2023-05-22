453656391450f909bbf4b2a1cef3629f2c60bdfb
Ana Clara Oliveira <ana.clara@lnls.br>
2023-05-22 11:32:36 -0300

    IDFF.ENH: add new IOC PVs and improve layout

--- pyqt-apps/siriushla/si_ap_idff/main.py
+++ pyqt-apps/siriushla/si_ap_idff/main.py
@@ -2,16 +2,21 @@
 
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QLabel, QGridLayout, QSizePolicy as QSzPlcy, \
-    QWidget, QGroupBox, QVBoxLayout
+    QWidget, QGroupBox, QVBoxLayout, QPushButton, QSpacerItem
+
+import qtawesome as qta
+
+from pydm.widgets import PyDMPushButton
 
 from siriuspy.envars import VACA_PREFIX as _VACA_PREFIX
 from siriuspy.namesys import SiriusPVName as _PVName
 from siriuspy.search import IDSearch
-from siriuspy.idff.csdev import IDFFConst
+from siriuspy.idff.csdev import IDFFConst, ETypes as IDFFEnums
 
 from ..util import connect_window
 from ..widgets import SiriusMainWindow, SiriusLabel, SiriusSpinbox, \
-    PyDMStateButton, SiriusLedState, PyDMLogLabel
+    PyDMStateButton, SiriusLedState, PyDMLogLabel, SiriusLedAlert
+from ..widgets.dialog import StatusDetailDialog
 from ..as_ps_control.control_widget.ControlWidgetFactory import \
     ControlWidgetFactory
 from ..as_ps_control import PSDetailWindow
@@ -30,7 +35,7 @@ class IDFFWindow(SiriusMainWindow):
         self._const = IDFFConst(idname)
         self._idffdata = IDSearch.conv_idname_2_idff(self.idname)
         self.device = _PVName(self._const.idffname)
-        self.dev_pref = self.device.substitute(prefix=prefix)
+        self.dev_pref = self.device.substitute(prefix='ana')
         self.setObjectName('IDApp')
         self.setWindowTitle(self.device)
         self.setWindowIcon(get_idff_icon())
@@ -45,10 +50,11 @@ class IDFFWindow(SiriusMainWindow):
         wid = QWidget()
         lay = QGridLayout(wid)
         lay.addWidget(self.title, 0, 0, 1, 2)
-        lay.addWidget(self._basicSettingsWidget(), 1, 0)
-        lay.addWidget(self._idStatusWidget(), 2, 0)
-        lay.addWidget(self._logWidget(), 3, 0)
-        lay.addWidget(self._corrsMonitorWidget(), 1, 1, 3, 1)
+        lay.addWidget(self._idStatusWidget(), 1, 0)
+        lay.addWidget(self._corrStatusWidget(), 2, 0)
+        lay.addWidget(self._basicSettingsWidget(), 3, 0)
+        lay.addWidget(self._logWidget(), 4, 0)
+        lay.addWidget(self._corrsMonitorWidget(), 1, 1, 4, 1)
         self.setCentralWidget(wid)
 
     def _basicSettingsWidget(self):
@@ -74,24 +80,72 @@ class IDFFWindow(SiriusMainWindow):
         self.lb_loopfreq = SiriusLabel(
             self, self.dev_pref.substitute(propty='LoopFreq-RB'))
 
-        ld_polar = QLabel(
-            'Polarization: ', self, alignment=Qt.AlignRight)
-        self.lb_polar = SiriusLabel(
-            self, self.dev_pref.substitute(propty='Polarization-Mon'))
+        ld_controlqs = QLabel(
+            'Control QS: ', self, alignment=Qt.AlignRight)
+        self.sb_controlqs = PyDMStateButton(
+            self, self.dev_pref.substitute(propty='ControlQS-Sel'))
+        self.lb_controlqs = SiriusLedState(
+            self, self.dev_pref.substitute(propty='ControlQS-Sts'))
+
+        ld_usepssofb = QLabel(
+            'Use PSSOFB: ', self, alignment=Qt.AlignRight)
+        self.sb_usepssofb = PyDMStateButton(
+            self, self.dev_pref.substitute(propty='SOFBMode-Sel'))
+        self.lb_usepssofb = SiriusLedState(
+            self, self.dev_pref.substitute(propty='SOFBMode-Sts'))
 
         gbox = QGroupBox('Settings', self)
         lay = QGridLayout(gbox)
         lay.addWidget(ld_configname, 0, 0)
-        lay.addWidget(self.le_configname, 0, 1)
-        lay.addWidget(self.lb_configname, 0, 2)
-        lay.addWidget(ld_loopstate, 1, 0)
-        lay.addWidget(self.sb_loopstate, 1, 1)
-        lay.addWidget(self.lb_loopstate, 1, 2)
-        lay.addWidget(ld_loopfreq, 2, 0)
-        lay.addWidget(self.sb_loopfreq, 2, 1)
-        lay.addWidget(self.lb_loopfreq, 2, 2)
-        lay.addWidget(ld_polar, 3, 0)
-        lay.addWidget(self.lb_polar, 3, 1, 1, 2)
+        lay.addWidget(self.le_configname, 0, 1, 1, 3)
+        lay.addWidget(self.lb_configname, 1, 1, 1, 3)
+        lay.addItem(QSpacerItem(0, 15, QSzPlcy.Ignored, QSzPlcy.Fixed), 2, 0)
+        lay.addWidget(ld_loopstate, 3, 0)
+        lay.addWidget(self.sb_loopstate, 3, 1)
+        lay.addWidget(self.lb_loopstate, 3, 2)
+        lay.addWidget(ld_loopfreq, 4, 0)
+        lay.addWidget(self.sb_loopfreq, 4, 1)
+        lay.addWidget(self.lb_loopfreq, 4, 2, 1, 2)
+        lay.addWidget(ld_controlqs, 5, 0)
+        lay.addWidget(self.sb_controlqs, 5, 1)
+        lay.addWidget(self.lb_controlqs, 5, 2)
+        lay.addItem(QSpacerItem(0, 15, QSzPlcy.Ignored, QSzPlcy.Fixed), 6, 0)
+        lay.addWidget(ld_usepssofb, 7, 0)
+        lay.addWidget(self.sb_usepssofb, 7, 1)
+        lay.addWidget(self.lb_usepssofb, 7, 2)
+        return gbox
+
+    def _corrStatusWidget(self):
+        ld_corconf = QLabel(
+            'Corr. Status: ', self, alignment=Qt.AlignRight)
+        self.led_corr = SiriusLedAlert(
+            self, self.dev_pref.substitute(propty='CorrStatus-Mon'))
+        pb_corsts = QPushButton('', self)
+        pb_corsts.setIcon(qta.icon('fa5s.list-ul'))
+        pb_corsts.setToolTip('Open Detailed Status View')
+        pb_corsts.setObjectName('sts')
+        pb_corsts.setStyleSheet(
+            '#sts{min-width:25px; max-width:25px; icon-size:20px;}')
+        connect_window(
+            pb_corsts, StatusDetailDialog, parent=self,
+            pvname=self.dev_pref.substitute(propty='CorrStatus-Mon'),
+            labels=IDFFEnums.STS_LBLS_CORR, section='ID',
+            title='Corrector Status')
+        self.pb_corconf = PyDMPushButton(
+            self, pressValue=1,
+            init_channel=self.dev_pref.substitute(propty='CorrConfig-Cmd'))
+        self.pb_corconf.setToolTip('Send PwrState and OpMode')
+        self.pb_corconf.setIcon(qta.icon('fa5s.sync'))
+        self.pb_corconf.setObjectName('conf')
+        self.pb_corconf.setStyleSheet(
+            '#conf{min-width:25px; max-width:25px; icon-size:20px;}')
+
+        gbox = QGroupBox('Corrector Status', self)
+        lay = QGridLayout(gbox)
+        lay.addWidget(ld_corconf, 0, 0)
+        lay.addWidget(self.led_corr, 0, 1)
+        lay.addWidget(pb_corsts, 0, 2)
+        lay.addWidget(self.pb_corconf, 0, 3)
         return gbox
 
     def _idStatusWidget(self):
@@ -107,12 +161,20 @@ class IDFFWindow(SiriusMainWindow):
         self._lb_kparam = SiriusLabel(self, kparam, keep_unit=True)
         self._lb_kparam.showUnits = True
 
+        ld_polar = QLabel(
+            'Polarization: ', self, alignment=Qt.AlignRight)
+        self.lb_polar = SiriusLabel(
+            self, self.dev_pref.substitute(propty='Polarization-Mon'))
+
         gbox = QGroupBox('ID Status', self)
         lay = QGridLayout(gbox)
         lay.addWidget(ld_pparam, 0, 0)
         lay.addWidget(self._lb_pparam, 0, 1)
         lay.addWidget(ld_kparam, 1, 0)
         lay.addWidget(self._lb_kparam, 1, 1)
+        lay.addItem(QSpacerItem(0, 15, QSzPlcy.Ignored, QSzPlcy.Fixed), 2, 0)
+        lay.addWidget(ld_polar, 3, 0)
+        lay.addWidget(self.lb_polar, 3, 1, 1, 3)
         return gbox
 
     def _logWidget(self):
