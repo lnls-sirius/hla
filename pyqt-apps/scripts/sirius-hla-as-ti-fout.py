304f432f3d8e987a92baed878804b1eca003af00
Ana Clara Oliveira <ana.clara@lnls.br>
2023-07-18 17:32:39 -0300

    ENH: add new scripts for launching individual timesys device windows

---
+++ pyqt-apps/scripts/sirius-hla-as-ti-fout.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python-sirius
+
+"""FOUT GUI."""
+
+import sys
+import argparse as _argparse
+
+import qtawesome as qta
+
+from siriuspy.envars import VACA_PREFIX
+
+from siriushla.sirius_application import SiriusApplication
+from siriushla.widgets.windows import create_window_from_widget
+from siriushla.as_ti_control import FOUT
+from siriushla.util import get_appropriate_color
+
+parser = _argparse.ArgumentParser(description="Run FOUT GUI.")
+parser.add_argument(
+    'device', type=str, help='Select a device')
+parser.add_argument(
+    '-p', "--prefix", type=str, default=VACA_PREFIX,
+    help="Define the prefix for the PVs in the window.")
+args = parser.parse_args()
+
+app = SiriusApplication(None, sys.argv)
+icon = qta.icon('mdi.timer', color=get_appropriate_color('AS'))
+window = create_window_from_widget(
+    FOUT, title=args.device, icon=icon, is_main=True)
+app.open_window(window, parent=None, device=args.device, prefix=args.prefix)
+sys.exit(app.exec_())
