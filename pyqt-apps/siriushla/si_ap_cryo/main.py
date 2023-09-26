68301846363d73c43ee3f5716107c5ccf7ee9099
Rafael Lyra <rafael.lyra@lnls.br>
2023-09-26 12:07:21 -0300

    Cryo screen 1

--- pyqt-apps/siriushla/si_ap_cryo/main.py
+++ pyqt-apps/siriushla/si_ap_cryo/main.py
@@ -1,8 +1,132 @@
+import os as _os
+from qtpy.QtCore import QEvent, Qt
+from qtpy.QtWidgets import QLabel, QSizePolicy, QGroupBox, \
+    QVBoxLayout
+from qtpy.QtGui import QPixmap
+
 from siriushla.widgets.windows import SiriusMainWindow
+from siriushla.widgets import RelativeWidget, SiriusLabel
 from siriuspy.envars import VACA_PREFIX as _VACA_PREFIX
+from .util import LABELS, PVS
 
 
 class CryoMain(SiriusMainWindow):
 
     def __init__(self, parent=None, prefix=_VACA_PREFIX):
         super().__init__(parent=parent)
+        self.prefix = prefix + ('-' if prefix else '')
+        self.relative_widgets = []
+        self._setupUi()
+
+    def eventFilter(self, obj, event):
+        """Signal the resize event to the relative Widgets"""
+        if (event.type() == QEvent.Resize):
+            for relative_item in self.relative_widgets:
+                relative_item.relativeResize()
+        return super().eventFilter(obj, event)
+
+    def add_image(self, img_path):
+        self.image_container = QLabel()
+        img_file = (img_path + ".png")
+        pixmap = QPixmap(_os.path.join(
+            _os.path.abspath(_os.path.dirname(__file__)), img_file))
+        self.image_container.setScaledContents(True)
+        self.image_container.setSizePolicy(
+            QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.image_container.setPixmap(pixmap)
+        self.image_container.setMinimumSize(1750, 850)
+        self.image_container.installEventFilter(self)
+
+        return self.image_container
+
+    def save_relative_widget(self, widget, size, coord):
+        """ Save relative widget to list """
+        rel_wid = RelativeWidget(
+            parent=self.image_container,
+            widget=widget,
+            relative_pos=coord + size)
+
+        self.relative_widgets.append(rel_wid)
+
+    def add_labels(self):
+        for text, config in LABELS.items():
+            if isinstance(config, tuple):
+                self.create_label(text, config[0])
+                config = config[1]
+            self.create_label(text, config)
+
+    def handle_highlight(self, config, lbl):
+        if isinstance(config, dict):
+            lbl.setStyleSheet("""
+                background-color: """+config["color"]+""";
+                color: #ffffff;
+                font-size: 12px;
+            """)
+
+            return config["position"]
+        return config
+
+    def create_label(self, text, config):
+        lbl = QLabel(text)
+        lbl.setAlignment(Qt.AlignCenter)
+        position = self.handle_highlight(config, lbl)
+        self.save_relative_widget(lbl, [7.5, 4], position)
+
+    def add_labels(self):
+        for text, config in LABELS.items():
+            if isinstance(config, tuple):
+                self.create_label(text, config[0])
+                config = config[1]
+            self.create_label(text, config)
+
+    def add_sirius_label(self, pvname, color):
+        pydm_lbl = SiriusLabel(init_channel=pvname)
+        pydm_lbl.showUnits = True
+
+        pydm_lbl.setStyleSheet("""
+            background-color: """+color+""";
+            color: #ffffff;
+            margin-top: 2px;
+        """)
+
+        return pydm_lbl
+
+    def create_pydm_group(self, config):
+        lay = QVBoxLayout()
+        lay.setContentsMargins(0, 0, 0, 0)
+
+        pvname = config["pvname"]
+        color = config["color"]
+        if isinstance(pvname, tuple):
+            pydm_lbl = self.add_sirius_label(
+                pvname[0], color)
+            lay.addWidget(pydm_lbl)
+            pvname = pvname[1]
+
+        pydm_lbl = self.add_sirius_label(
+            pvname, color)
+        lay.addWidget(pydm_lbl)
+
+        return lay
+
+    def add_pvs(self):
+        for text, config in PVS.items():
+            group = QGroupBox()
+            group.setTitle(text)
+
+            lay = self.create_pydm_group(config)
+            group.setLayout(lay)
+
+            if isinstance(config["pvname"], tuple):
+                height = 10
+            else:
+                height = 7
+            self.save_relative_widget(
+                group, [7.5, height], config["position"])
+
+    def _setupUi(self):
+        bg_img = self.add_image("cryo1")
+        self.add_labels()
+        self.add_pvs()
+
+        self.setCentralWidget(bg_img)
--- pyqt-apps/siriushla/si_ap_cryo/util.py
+++ pyqt-apps/siriushla/si_ap_cryo/util.py
@@ -1,104 +1,245 @@
+class COLOR:
+    black = "#000000"
+    dark_red = "#6b1c40"
+    dark_blue = "#325692"
+    blue = "#0000ff"
+    gold = "#808000"
+
+
 LABELS = {
-    "tag": {
-        "CP1.CW1.GQ050": [0, 0],
-        "Master Mode": [0, 0],
-        "CP2.CW1.GQ050": [0, 0],
-        "OC1.HP1.HQ010": [0, 0],
-        "OC1.HP1.HQ020": [0, 0],
-        "OC1.HP1.010L": [0, 0],
-        "OC1.HP1.020L": [0, 0],
-        "TUI.QN020": [0, 0],
-        "OC2.HP1.HQ010": [0, 0],
-        "OC2.HP1.HQ020": [0, 0],
-        "OC2.HP1.010L": [0, 0],
-        "OC2.HP1.020L": [0, 0],
-        "OC1.HP1.HP050": [0, 0],
-        "LS1.010.U.LC": [
-            [0, 0], [0, 0]],
-        "OC2.HP1.HP050": [0, 0],
-        "Detector": [0, 0],
-        "Coldbox": [
-            [0, 0], [0, 0]],
-        "BS1.CM030": [0, 0],
-        "BS2.CM030": [0, 0],
-        "BS3.CM030": [0, 0]
+    "CP1.CW1.GQ050": [11.25, 11],
+    "Master Mode": [10.5, 20],
+    "CP2.CW1.GQ050": [11.25, 54],
+    "OC1.HP1.HQ010": [28.5, 1],
+    "OC1.HP1.HQ020": [35.5, 1],
+    "OC1.HP1.010.L": [31.75, 12],
+    "OC1.HP1.020.L": [38.75, 12],
+    "TUI.QN020": [31.5, 29.5],
+    "OC2.HP1.HQ010": [28.5, 43],
+    "OC2.HP1.HQ020": [35.5, 43],
+    "OC2.HP1.010.L": [31.75, 54],
+    "OC2.HP1.020.L": [38.75, 54],
+    "OC1.HP1.HP050": [45.25, 1],
+    "LS1.010.U.LC": (
+        [63, 7], [63, 49]),
+    "OC2.HP1.HP050": [45.25, 43],
+    "Detector": [51, 75],
+    "Coldbox": (
+        [91.75, 10], [91.75, 92]),
+    "BS1.CM030": [91.75, 39],
+    "BS2.CM030": [91.75, 57],
+    "BS3.CM030": [91.75, 75],
+    "CI1.LP1.016.U.PC": {
+        "position": [4.5, 36],
+        "color": COLOR.blue
     },
-    "highlight_tag": {
-        "CI1.LP1.016UPC": {
-            "position": [0, 0],
-            "color": "blue"
-        },
-        "CI1.LP1.015UPC": {
-            "position": [0, 0],
-            "color": "blue"
-        },
-        "CI2.LP1.016UPC": {
-            "position": [0, 0],
-            "color": "blue"
-        },
-        "CI2.LP1.015UPC": {
-            "position": [0, 0],
-            "color": "blue"
-        },
-        "OC1.HP1.QM010": {
-            "position": [0, 0],
-            "color": "dark red"
-        },
-        "OC1.HP1.QM020": {
-            "position": [0, 0],
-            "color": "dark red"
-        },
-        "CW1.050.U.SC": {
-            "position": [0, 0],
-            "color": "gold"
-        },
-        "CW1.052.U.SC": {
-            "position": [0, 0],
-            "color": "gold"
-        },
-        "OC2.HP1.QM010": {
-            "position": [0, 0],
-            "color": "dark red"
-        },
-        "OC2.HP1.QM020": {
-            "position": [0, 0],
-            "color": "dark red"
-        },
-        "GP1.HP1.010.U.PC": {
-            "position": [0, 0],
-            "color": "blue"
-        },
-        "GP2.HP1.010.U.PC": {
-            "position": [0, 0],
-            "color": "blue"
-        },
-        "MC1.HP1.QM010": {
-            "position": [0, 0],
-            "color": "dark blue"
-        },
-        "MC1.HP2.QM010": {
-            "position": [0, 0],
-            "color": "dark blue"
-        },
-        "MC1.QM040": {
-            "position": [0, 0],
-            "color": "dark blue"
-        },
-        "MC1.LP1.QM010": {
-            "position": [0, 0],
-            "color": "dark blue"
-        },
-        "MC1.LP2.QM010": {
-            "position": [0, 0],
-            "color": "dark blue"
-        },
-        "GP1.HP1.051.U.GC": {
-            "position": [0, 0],
-            "color": "black"
-        },
-        "GP2.HP1.051.U.GC": {
-            "position": [0, 0],
-            "color": "black"
-        }
+    "CI1.LP1.015.U.PC": {
+        "position": [4.5, 38.75],
+        "color": COLOR.blue
+    },
+    "CI2.LP1.016.U.PC": {
+        "position": [16, 74],
+        "color": COLOR.blue
+    },
+    "CI2.LP1.015.U.PC": {
+        "position": [16, 77],
+        "color": COLOR.blue
+    },
+    "OC1.HP1.QM010": {
+        "position": [31.75, 21.85],
+        "color": COLOR.dark_red
+    },
+    "OC1.HP1.QM020": {
+        "position": [39.25, 21.85],
+        "color": COLOR.dark_red
+    },
+    "CW1.050.U.SC": {
+        "position": [25.25, 26],
+        "color": COLOR.gold
+    },
+    "CW1.052.U.SC": {
+        "position": [25.25, 31],
+        "color": COLOR.gold
+    },
+    "OC2.HP1.QM010": {
+        "position": [31.75, 63.85],
+        "color": COLOR.dark_red
+    },
+    "OC2.HP1.QM020": {
+        "position": [39.25, 63.85],
+        "color": COLOR.dark_red
+    },
+    "GP1.HP1.010.U.PC": {
+        "position": [58.5, 9],
+        "color": COLOR.blue
+    },
+    "GP2.HP1.010.U.PC": {
+        "position": [58.5, 51],
+        "color": COLOR.blue
+    },
+    "MC1.HP1.QM010": {
+        "position": [43.25, 59],
+        "color": COLOR.dark_blue
+    },
+    "MC1.HP2.QM010": {
+        "position": [56.25, 59],
+        "color": COLOR.dark_blue
+    },
+    "MC1.QM040": {
+        "position": [52.5, 69],
+        "color": COLOR.dark_blue
+    },
+    "MC1.LP1.QM010": {
+        "position": [43.25, 84],
+        "color": COLOR.dark_blue
+    },
+    "MC1.LP2.QM010": {
+        "position": [56.25, 84],
+        "color": COLOR.dark_blue
+    },
+    "GP1.HP1.051.U.GC": {
+        "position": [71, 26.5],
+        "color": COLOR.black
+    },
+    "GP2.HP1.051.U.GC": {
+        "position": [71, 68.5],
+        "color": COLOR.black
+    }
+}
+
+PVS = {
+    "CP1.CW1.035.T": {
+        "position": [5, 10],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_red
+    },
+    "CP1.CW1.065.P": {
+        "position": [17.5, 5.25],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.blue
+    },
+    "CP1.CW1.065.T": {
+        "position": [17.5, 10],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_red
+    },
+    "CP1.CW1.GQ050": {
+        "position": [16, 17],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.black
+    },
+    "CP1.CW1.050.S": {
+        "position": [16, 22],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.gold
+    },
+    "CP1.LP1.015.P": {
+        "position": [4.5, 27],
+        "pvname": (
+            "RAD:Thermo1:Gamma", "RAD:Thermo1:Gamma"),
+        "color": COLOR.blue
+    },
+    "CP2.CW1.035.T": {
+        "position": [5, 53],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_red
+    },
+    "CP2.CW1.065.P": {
+        "position": [17.5, 48.25],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.blue
+    },
+    "CP2.CW1.065.T": {
+        "position": [17.5, 53],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_red
+    },
+    "CP2.CW1.GQ050": {
+        "position": [16, 60],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.black
+    },
+    "CP2.CW1.050.S": {
+        "position": [16, 65],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.gold
+    },
+    "CP2.LP1.015.P": {
+        "position": [16, 79],
+        "pvname": (
+            "RAD:Thermo1:Gamma", "RAD:Thermo1:Gamma"),
+        "color": COLOR.blue
+    },
+    "GP1.HP1.010.P": {
+        "position": [51.75, 3],
+        "pvname": (
+            "RAD:Thermo1:Gamma", "RAD:Thermo1:Gamma"),
+        "color": COLOR.blue
+    },
+    "GP2.HP1.010.P": {
+        "position": [51.75, 45],
+        "pvname": (
+            "RAD:Thermo1:Gamma", "RAD:Thermo1:Gamma"),
+        "color": COLOR.blue
+    },
+    "MC1.051.A": {
+        "position": [37, 72],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.gold
+    },
+    "MC1.052.A": {
+        "position": [44, 72],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.gold
+    },
+    "MC1.053.A": {
+        "position": [37, 77],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.gold
+    },
+    "MC1.054.A": {
+        "position": [44, 77],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.gold
+    },
+    "GP1.HP1.QN030": {
+        "position": [71.5, 18],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_blue
+    },
+    "GP1.HP1.QN050": {
+        "position": [71.5, 30.5],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_blue
+    },
+    "GP1.HP1.QN070": {
+        "position": [71.5, 37],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_blue
+    },
+    "GP1.HP1.040.P": {
+        "position": [78.5, 21],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.blue
+    },
+    "GP2.HP1.QN030": {
+        "position": [71.5, 60],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_blue
+    },
+    "GP2.HP1.QN050": {
+        "position": [71.5, 72.5],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_blue
+    },
+    "GP2.HP1.QN070": {
+        "position": [71.5, 79],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.dark_blue
+    },
+    "GP2.HP1.040.P": {
+        "position": [78.5, 56.5],
+        "pvname": "RAD:Thermo1:Gamma",
+        "color": COLOR.blue
     }
 }
