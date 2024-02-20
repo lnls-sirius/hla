1e3e12a5ac16be1d0843640b56522e78b8499f4f
Ana Clara Oliveira <ana.clara@lnls.br>
2024-02-20 20:33:02 -0300

    LiEgun.FIX: fix conditions according to RAD page documentation

--- pyqt-apps/siriushla/li_eg_control/custom_widgets.py
+++ pyqt-apps/siriushla/li_eg_control/custom_widgets.py
@@ -22,33 +22,40 @@ class LIEGTrigEnblDetail(SiriusDialog):
 
     def _setupUi(self):
         desc2cond = {
-            "LI-01:EG-TriggerPS is enable": {
-                "LI-01:EG-TriggerPS:enable": _PSConst.OffOn.On
+            1: {
+                "LI-01:EG-TriggerPS is enable": {
+                    "LI-01:EG-TriggerPS:enable": _PSConst.OffOn.On
+                },
+                "LI-01:PS-Spect Current is out of operation range": {
+                    "LI-01:PS-Spect:Current-Mon": {
+                        "comp": "ou",
+                        "value": [0.5, 12.0],
+                    }
+                },
             },
-            "LI-01:PS-Spect Current is within operation range": {
-                "LI-01:PS-Spect:Current-Mon": {
-                    "comp": "ou",
-                    "value": [0.5, 12],
-                }
-            },
-            "TB-Fam:PS-B OpMode is in SlowRef": {
-                "TB-Fam:PS-B:OpMode-Sts": _PSConst.States.SlowRef
-            },
-            "TB-Fam:PS-B Current is in operation range": {
-                "TB-Fam:PS-B:Current-Mon": {
-                    "comp": "ge",
-                    "value": 5,
-                }
-            },
-            "TB-04:PU-InjSept Pulse is On": {
-                "TB-04:PU-InjSept:Pulse-Sts": _PSConst.OffOn.On
-            },
-            "TB-04:PU-InjSept Voltage is in operation range": {
-                "TB-04:PU-InjSept:Voltage-Mon": {
-                    "comp": "ge",
-                    "value": 400,
-                }
-            },
+            2: {
+                "LI-01:EG-TriggerPS is enable": {
+                    "LI-01:EG-TriggerPS:enable": _PSConst.OffOn.On
+                },
+                "TB-Fam:PS-B OpMode is in SlowRef": {
+                    "TB-Fam:PS-B:OpMode-Sts": _PSConst.States.SlowRef
+                },
+                "TB-Fam:PS-B Current is greater than 5A": {
+                    "TB-Fam:PS-B:Current-Mon": {
+                        "comp": "ge",
+                        "value": 5.0,
+                    }
+                },
+                "TB-04:PU-InjSept Pulse is On": {
+                    "TB-04:PU-InjSept:Pulse-Sts": _PSConst.OffOn.On
+                },
+                "TB-04:PU-InjSept Voltage is greater than 400V": {
+                    "TB-04:PU-InjSept:Voltage-Mon": {
+                        "comp": "ge",
+                        "value": 400.0,
+                    }
+                },
+            }
         }
         lay = QGridLayout(self)
 
@@ -56,9 +63,13 @@ class LIEGTrigEnblDetail(SiriusDialog):
         lay.addWidget(title, 0, 0, 1, 2)
 
         row = 1
-        for desc, cond in desc2cond.items():
-            label = QLabel(desc, self)
-            led = PyDMLedMultiChannel(self, channels2values=cond)
-            lay.addWidget(led, row, 0)
-            lay.addWidget(label, row, 1)
-            row += 1
+        for group, conds in desc2cond.items():
+            if group > 1:
+                lay.addWidget(QLabel('<h4>or</h4>'), row, 1, 1, 2)
+                row += 1
+            for desc, cond in conds.items():
+                label = QLabel(desc, self)
+                led = PyDMLedMultiChannel(self, channels2values=cond)
+                lay.addWidget(led, row, 0)
+                lay.addWidget(label, row, 1)
+                row += 1
