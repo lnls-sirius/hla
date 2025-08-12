fe00f230b2f86a35737782b3450ec35bec3c6992
Ximenes Rocha Resende <xresende@gmail.com>
2025-08-12 09:53:22 -0300

    Add a code implementation suggestion in PSDetailWindow

--- pyqt-apps/siriushla/as_ps_control/PSDetailWindow.py
+++ pyqt-apps/siriushla/as_ps_control/PSDetailWindow.py
@@ -14,7 +14,7 @@ from siriushla.widgets import SiriusMainWindow
 from .detail_widget.DetailWidgetFactory import DetailWidgetFactory
 
 _bbbiptext, _ = read_text_data(beaglebone_ip_list())
-BBBNAME2IP = {item[0]: item[1] for item in _bbbiptext}
+BBBNAME2IP = {name: ip for name, ip _bbbiptext}
 
 
 class PSDetailWindow(SiriusMainWindow):
