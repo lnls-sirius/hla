6bc4f4724bed9f63ea5fcca54dcf090f5b6d64a8
Ximenes Rocha Resende <xresende@gmail.com>
2026-04-02 09:55:45 -0300

    DEP: (DEV.SCRN) Remove TB-04:DI-Scrn from device

--- pyqt-apps/siriushla/as_di_scrns/list_scrns.py
+++ pyqt-apps/siriushla/as_di_scrns/list_scrns.py
@@ -9,9 +9,12 @@ from siriushla import util
 
 def get_scrn_list(sec):
     if sec == 'TB':
+        # NOTE: TB-04:DI-Scrn was removed during the March 2026 shutdown
+        # as part of septum-related modifications to mitigate
+        # secondary current loops induced by its pulsed field.
         return ['TB-01:DI-Scrn-1', 'TB-01:DI-Scrn-2',
                 'TB-02:DI-Scrn-1', 'TB-02:DI-Scrn-2',
-                'TB-03:DI-Scrn', 'TB-04:DI-Scrn']
+                'TB-03:DI-Scrn']
     elif sec == 'BO':
         return ['BO-01D:DI-Scrn-1', 'BO-01D:DI-Scrn-2',
                 'BO-02U:DI-Scrn']
