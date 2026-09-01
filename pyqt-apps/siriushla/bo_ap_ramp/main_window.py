33cfab657f8eab675dfaa5fd99d0e770e5325c49
rafael.lyra <rafael.lyra@lnls.br>
2026-09-01 13:31:26 -0300

    Fix QUndoStack import
    This class should be imported from QtGui instead of
    QtWidgets

--- pyqt-apps/siriushla/bo_ap_ramp/main_window.py
+++ pyqt-apps/siriushla/bo_ap_ramp/main_window.py
@@ -3,9 +3,9 @@
 from copy import deepcopy as _dcopy
 
 from qtpy.QtCore import Qt, Slot, Signal
-from qtpy.QtGui import QKeySequence, QPalette
+from qtpy.QtGui import QKeySequence, QPalette, QUndoStack
 from qtpy.QtWidgets import QLabel, QWidget, QGridLayout, \
-    QUndoStack, QMessageBox
+    QMessageBox
 import qtawesome as qta
 
 from siriuspy.ramp import ramp
